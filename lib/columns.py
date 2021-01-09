210431d5629c1db9feef8e8a47c8e0d2a3bd1277
Khoi Pham <pckhoi@gmail.com>
2021-01-09 18:20:09 +0700

    Extract names from Baton Rouge PD CPRR

--- lib/columns.py
+++ lib/columns.py
@@ -1,10 +1,28 @@
 PERSONEL_COLUMNS = [
-    "Employee ID", "Last Name", "Middle Name", "First Name", "Badge #",
+    "Last Name", "Middle Name", "First Name", "Badge #",
     "Employee ID #", "Department #", "Department", "Rank Number #", "Rank",
     "Birth Date", "Birth Year", "Hire Date", "Termination Date",
     "Pay Prog Start Date", "Annual Salary", "Hourly Salary"]
+
+COMPLAINT_COLUMNS = [
+    "Last Name", "First Name", "Middle Name", "Badge #", "Employee ID #", "Incident Type",
+    "Complaint Tracking Number", "Date Complaint Occurred",
+    "Date Complaint Received", "Date Complaint Investigation Complete",
+    "Investigation Status", "Disposition", "Complaint Classification",
+    "Bureau of Complainant", "Division of Complainant", "Unit of Complainant",
+    "Unit Additional Details of Complainant", "Working Status of Complainant",
+    "Shift of Complainant", "Rule Violation", "Paragraph Violation",
+    "Unique Officer Allegation ID", "Officer Race Ethnicity", "Officer Gender",
+    "Officer Age", "Officer Years of Service", "Complainant Gender",
+    "Complainant Ethnicity", "Complainant Age"
+]
 
 
 def rearrange_personel_columns(df):
     existing_cols = set(df.columns)
     return df[[col for col in PERSONEL_COLUMNS if col in existing_cols]]
+
+
+def rearrange_complaint_columns(df):
+    existing_cols = set(df.columns)
+    return df[[col for col in COMPLAINT_COLUMNS if col in existing_cols]]
--- lib/new_orleans_pd_pprr.py
+++ lib/new_orleans_pd_pprr.py
@@ -6,7 +6,7 @@ def remove_unnamed_cols(df):
 
 
 def clean_pprr(orig_df):
-    df = orig_df[["Birthdate - Personal Dta", "Department ID - Job Dta",
+    df = orig_df[["Birthdate - Personal Dta", "Department ID - Job Dta", "Empl ID",
                   "Badge Nbr", "Job Code - Job Dta", "Description - Job Code Info"]]
     name_df = orig_df["Name - Personal Dta"].str.split(",", expand=True)
     df.loc[:, "Last Name"] = name_df.iloc[:, 0]
@@ -18,6 +18,7 @@ def clean_pprr(orig_df):
         "Department ID - Job Dta": "Department #",
         "Badge Nbr": "Badge #",
         "Job Code - Job Dta": "Rank Number #",
-        "Description - Job Code Info": "Rank"
+        "Description - Job Code Info": "Rank",
+        "Empl ID": "Employee ID #"
     }, inplace=True)
     return rearrange_personel_columns(df)
