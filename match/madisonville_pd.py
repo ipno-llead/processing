510a0b732e13742ee713e8510685ba38e0fbf67c
Khoi Pham <pckhoi@gmail.com>
2021-02-25 15:33:31 +0700

    update match script for madisonville

--- match/madisonville_pd.py
+++ match/madisonville_pd.py
@@ -1,6 +1,5 @@
 from lib.match import (
-    match_records, ThresholdClassifier, NoopIndex, JaroWinklerSimilarity,
-    print_match_result
+    ThresholdMatcher, NoopIndex, JaroWinklerSimilarity
 )
 from lib.path import data_file_path, ensure_data_dir
 import pandas as pd
@@ -8,30 +7,20 @@ import sys
 sys.path.append("../")
 
 
-def match(cprr, pprr, print_result=False):
+def match(cprr, pprr):
     dfa = cprr[["last_name", "first_name"]]
     dfb = pprr[["last_name", "first_name", "uid"]].drop_duplicates()
     dfb = dfb.set_index("uid", drop=True)
-    matches, potential_matches, non_matches = match_records(
-        NoopIndex(),
-        ThresholdClassifier(
-            fields={
-                "first_name": JaroWinklerSimilarity(),
-                "last_name": JaroWinklerSimilarity()
-            },
-            thresholds=[1, 0.7]
-        ),
-        dfa,
-        dfb
-    )
+    matcher = ThresholdMatcher(dfa, dfb, NoopIndex(), {
+        "first_name": JaroWinklerSimilarity(),
+        "last_name": JaroWinklerSimilarity()
+    })
+    matches = matcher.get_index_pairs_within_thresholds(lower_bound=1)
 
-    if print_result:
-        print_match_result(dfa, dfb, matches, potential_matches, non_matches)
-    else:
-        for idx, uid in matches:
-            cprr.loc[idx, "uid"] = uid
-        cprr = cprr.drop(columns=["first_name", "last_name"])
-        return cprr
+    for idx, uid in matches:
+        cprr.loc[idx, "uid"] = uid
+    cprr = cprr.drop(columns=["first_name", "last_name"])
+    return cprr
 
 
 if __name__ == "__main__":
