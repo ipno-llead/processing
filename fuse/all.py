b3d6999da97b4fe84b5c696bca775f0fdc5cb3ab
Khoi Pham <pckhoi@gmail.com>
2021-04-08 10:50:23 +0700

    update fuse all script

--- fuse/all.py
+++ fuse/all.py
@@ -9,29 +9,33 @@ sys.path.append("../")
 
 def fuse_personnel():
     return rearrange_personnel_columns(pd.concat([
-        pd.read_csv(data_file_path("fuse/per_new_orleans_pd.csv")),
-        pd.read_csv(data_file_path("fuse/per_new_orleans_harbor_pd.csv")),
         pd.read_csv(data_file_path("fuse/per_baton_rouge_pd.csv")),
         pd.read_csv(data_file_path("fuse/per_baton_rouge_so.csv")),
-        pd.read_csv(data_file_path("fuse/per_new_orleans_csd.csv")),
-        pd.read_csv(data_file_path("fuse/per_baton_rouge_csd.csv")),
+        pd.read_csv(data_file_path("fuse/per_new_orleans_harbor_pd.csv")),
+        pd.read_csv(data_file_path("fuse/per_new_orleans_pd.csv")),
         pd.read_csv(data_file_path("fuse/per_brusly_pd.csv")),
         pd.read_csv(data_file_path("fuse/per_port_allen_pd.csv")),
         pd.read_csv(data_file_path("fuse/per_madisonville_pd.csv")),
+        pd.read_csv(data_file_path("fuse/per_greenwood_pd.csv")),
+        pd.read_csv(data_file_path("fuse/per_st_tammany_so.csv")),
+        pd.read_csv(data_file_path("fuse/per_plaquemines_so.csv")),
+        pd.read_csv(data_file_path("fuse/per_louisiana_state_police.csv")),
     ]))
 
 
 def fuse_personnel_history():
     return rearrange_personnel_history_columns(pd.concat([
-        pd.read_csv(data_file_path("fuse/perhist_new_orleans_pd.csv")),
-        pd.read_csv(data_file_path("fuse/perhist_new_orleans_harbor_pd.csv")),
         pd.read_csv(data_file_path("fuse/perhist_baton_rouge_pd.csv")),
         pd.read_csv(data_file_path("fuse/perhist_baton_rouge_so.csv")),
-        pd.read_csv(data_file_path("fuse/perhist_new_orleans_csd.csv")),
-        pd.read_csv(data_file_path("fuse/perhist_baton_rouge_csd.csv")),
+        pd.read_csv(data_file_path("fuse/perhist_new_orleans_harbor_pd.csv")),
+        pd.read_csv(data_file_path("fuse/perhist_new_orleans_pd.csv")),
         pd.read_csv(data_file_path("fuse/perhist_brusly_pd.csv")),
         pd.read_csv(data_file_path("fuse/perhist_port_allen_pd.csv")),
         pd.read_csv(data_file_path("fuse/perhist_madisonville_pd.csv")),
+        pd.read_csv(data_file_path("fuse/perhist_greenwood_pd.csv")),
+        pd.read_csv(data_file_path("fuse/perhist_st_tammany_so.csv")),
+        pd.read_csv(data_file_path("fuse/perhist_plaquemines_so.csv")),
+        pd.read_csv(data_file_path("fuse/perhist_louisiana_state_police.csv")),
     ]))
 
 
@@ -39,9 +43,15 @@ def fuse_complaint():
     return rearrange_complaint_columns(pd.concat([
         pd.read_csv(data_file_path("fuse/com_baton_rouge_pd.csv")),
         pd.read_csv(data_file_path("fuse/com_baton_rouge_so.csv")),
+        pd.read_csv(data_file_path("fuse/com_new_orleans_harbor_pd.csv")),
         pd.read_csv(data_file_path("fuse/com_brusly_pd.csv")),
         pd.read_csv(data_file_path("fuse/com_port_allen_pd.csv")),
         pd.read_csv(data_file_path("fuse/com_madisonville_pd.csv")),
+        pd.read_csv(data_file_path("fuse/com_greenwood_pd.csv")),
+        pd.read_csv(data_file_path("fuse/com_new_orleans_pd.csv")),
+        pd.read_csv(data_file_path("fuse/com_st_tammany_so.csv")),
+        pd.read_csv(data_file_path("fuse/com_plaquemines_so.csv")),
+        pd.read_csv(data_file_path("fuse/complaint.csv")),
     ]))
 
 
