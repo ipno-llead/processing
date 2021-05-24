bf8ac898e39621867b9d02c2b180b93a1ba236cf
ayyubibrahim <ayyub.ibrahimi@gmail.com>
2021-05-23 19:39:09 -0500

    split disposition and actions, cleaned dates, and cleaned charges

--- clean/scott_pd_cprr.py
+++ clean/scott_pd_cprr.py
@@ -2,7 +2,7 @@ from lib.columns import clean_column_names
 from lib.uid import gen_uid
 from lib.path import data_file_path, ensure_data_dir
 from lib.clean import (
-    clean_names, standardize_desc_cols, clean_dates, clean_sexes, clean_races, clean_datetimes
+    clean_names, standardize_desc_cols, clean_dates
 )
 import pandas as pd
 import sys
@@ -16,6 +16,21 @@ def split_name(df):
     df = df.drop(columns="full_name")
     return df
 
+
+def split_disposition_action(df):
+    outcomes = df["disposition/action"].str.split("/", expand=True)
+    df.loc[:, "disposition"] = outcomes.loc[:, 0].fillna("")
+    df.loc[:, "action"] = outcomes.loc[:, 1].fillna("")
+    df = df.drop(columns="disposition/action")
+    return df
+
+def clean_rank(df):
+    df.loc[:, "rank_desc"] = df.rank_desc.str.lower().str.strip() \
+        .str.replace("pofc", "officer", regex=False)\
+        .str.replace("lt.", "lieutenant ", regex=False)
+    return df
+
+
 def assign_agency(df):
     df.loc[:, "agency"] = "Scott PD"
     return df
@@ -24,14 +39,12 @@ def assign_prod_year(df, year):
     df.loc[:, "data_production_year"] = year
     return df
 
-
 def clean_charges(df):
     df.loc[:, "charges"] = df.charges.str.lower().str.strip() \
         .str.replace("conduct unbecoming/ rude behavior", "conduct unbecoming (rude behavior)", regex=False)\
         .str.replace("conduct unbecoming/lying", "conduct unbecoming (lying)", regex=False)\
         .str.replace("conduct unbecoming/rudeness", "conduct unbecoming (rudeness)", regex=False)
     return df
-
 
 def clean():
     df = pd.read_csv(data_file_path(
@@ -46,12 +59,18 @@ def clean():
         .pipe(
             standardize_desc_cols,
             ["rank_desc", "disposition/action"])\
-        .pipe(assign_prod_year, '2020')\
+        .pipe(assign_prod_year, "2020")\
         .pipe(assign_agency)\
         .pipe(clean_charges)\
-        .pipe(clean_names, ['first_name', 'last_name'])\
+        .pipe(clean_rank)\
+        .pipe(split_disposition_action)\
+        .pipe(clean_dates, ["notification_date"])\
+        .pipe(clean_names, ["first_name", "last_name"])\
         .pipe(gen_uid, ["agency", "first_name", "last_name"])\
-        .pipe(gen_uid, ['agency', 'uid'], 'complaint_uid')
+        .pipe(gen_uid, ["agency", "uid", "notification_year", "notification_month", "notification_day"], "complaint_uid")
+    df = df[["first_name", "last_name", "rank_desc", "charges", "disposition",
+             "action", "notification_year","notification_month", "notification_day",
+             "agency", "data_production_year", "uid", "complaint_uid"]]
     return df
 
 if __name__ == "__main__":
