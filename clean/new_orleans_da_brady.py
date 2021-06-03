1515a72a8216281d015753a8d83e00a8c5977bc1
ayyubibrahimi <57520778+ayyubibrahimi@users.noreply.github.com>
2021-06-03 15:05:28 -0500

    cleaning new_orleans_da_cprr_2021.csv

--- clean/new_orleans_da_brady.py
+++ clean/new_orleans_da_brady.py
@@ -38,6 +38,60 @@ def clean_disposition(df):
     return df
 
 
+def clean_directive(df):
+    df.loc[:, 'directive'] = df.directive.str.lower().str.strip()\
+        .str.replace('\\', '/', regex=False)\
+        .str.replace('reltive ', 'relative', regex=False)\
+        .str.replace('licene', 'license', regex=False)\
+        .str.replace('r.s./', 'r.s. ', regex=False)\
+        .str.replace('r.s.1', 'r.s. ', regex=False)\
+        .str.replace("r.s.'", 'r.s. ', regex=False)\
+        .str.replace('r.s:', 'r.s. ', regex=False)\
+        .str.replace('la r.s.', 'la. r.s.')\
+        .str.replace('la s.', 'la. r.s. ')\
+        .str.replace('8attery', 'battery', regex=False)\
+        .str.replace('vehivle', 'vehicle', regex=False)\
+        .str.replace('drivers', "driver's", regex=False)\
+        .str.replace('32:79-driving', '32:79 driving', regex=False)\
+        .str.replace('relative public payroll fraud', 'relative to public payroll fraud', regex=False)\
+        .str.replace('to la r.s.', 'to wit: la r.s.', regex=False)\
+        .str.replace('relativeto', 'relative to', regex=False)\
+        .str.replace('4.108.1 resisting an officer/obstruction',
+                     '4:108.1 relative to resisting an officer/obstruction', regex=False)\
+        .str.replace('to wit 14:98', 'to wit: r.s. 14:98 relative to operating a vehicle while intoxicated ', regex=False)\
+        .str.replace('reckles', 'reckless', regex=False)\
+        .str.replace('r.s 32:81 following vehicles', 'r.s. 32:81 relative to following vehicles', regex=False)\
+        .str.replace('r.s. 32:58 careless operation',
+                     'r.s. 32:58 relative to careless operation of a moveable', regex=False)\
+        .str.replace('to wit: hit ad run', 'to wit: r.s. 14:100 hit and run driving', regex=False)\
+        .str.replace('to wit: simple battery', 'to wit: la. r.s. 14:35 relative to simple battery', regex=False)\
+        .str.replace('to wit:14:98', 'to wit: r.s. 14:98 relative to operating a vehicle while intoxicated', regex=False)\
+        .str.replace('to wit r.s. la 32:865', 'to wit: r.s. la 32:865')\
+        .str.replace('to wit 32.863.1', 'to wit: r.s. 32:863.1')\
+        .str.replace('to wit r.s. 4:80 relative to felony carnel knowledge of a juvenile',
+                     'to wit: r.s. 4:80 relative to felony carnel knowledge of a juvenile', regex=False)\
+        .str.replace('to wit r.s. 32:81 relative to following vehicles',
+                    'to wit: r.s. 32:81 relative to following vehicles', regex=False)\
+        .str.replace('to wit r.s. 32:58 relative to careless operation of a moveable',
+                     'to wit: r.s. 32:58 relative to careless operation of a moveable', regex=False)\
+        .str.replace('to wit 32.863.1', 'to wit 32.863.1', regex=False)\
+        .str.replace('14:98 driving while intoxicated',
+                     '14:98 relative to operating a vehicle while intoxicated', regex=False)\
+        .str.replace('14:98.3 d.w.i 3rd offense',
+                     'r.s. 14:98 relative to operating a vehicle while intoxicated (3rd offense)', regex=False)\
+        .str.replace('14:100 hit and run driving', '14:100 relative to hit and run driving', regex=False)\
+        .str.replace('14:133 filing or maintaining false public records',
+                     '14:133 relative to filing or maintaining false public records', regex=False)\
+        .str.replace('32:51 no vehicle license', '32:51 relative to no vehicle license', regex=False)\
+        .str.replace('32:61 maximum speed limit', '32:61 relative to maximum speed limit', regex=False)\
+        .str.replace('32:50 careless operation of a motor vehicle',
+                     '32:50 relative to careless operation of a motor vehicle', regex=False)\
+        .str.replace('14:138 public payroll fraud', '14:138 relative to public payroll fraud', regex=False)\
+        .str.replace('14:38 simple assault and r.s. 14:63 criminal trespassing',
+                     '14:38 relative to simple assault | r.s. 14:63 criminal trespassing', regex=False)
+    return df
+
+
 def clean():
     df = pd.read_csv(data_file_path(
         'new_orleans_da/new_orleans_da_cprr_2021.csv'))
@@ -53,6 +107,7 @@ def clean():
         .pipe(extract_date_from_pib)\
         .pipe(combine_rule_and_paragraph)\
         .pipe(clean_disposition)\
+        .pipe(clean_directive)\
         .pipe(clean_names, ['first_name', 'last_name'])\
         .pipe(set_values, {
             'data_production_year': 2021,
