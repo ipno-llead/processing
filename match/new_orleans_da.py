2fd49609e0b702b567b15d57077d427e5adfa39e
ayyubibrahimi <57520778+ayyubibrahimi@users.noreply.github.com>
2021-06-07 18:25:24 -0500

    cleaning new_orleans_da_cprr_2021.csv; added new_orleans_da_cprr_2021 to download_input; started the processs of matching cprr with pprr and post

--- match/new_orleans_da.py
+++ match/new_orleans_da.py
@@ -1,14 +1,64 @@
-from datamatch import ColumnsIndex, JaroWinklerSimilarity, ThresholdMatcher
+from datamatch import ThresholdMatcher, JaroWinklerSimilarity, ColumnsIndex
 from lib.path import data_file_path, ensure_data_dir
 import pandas as pd
 import sys
 sys.path.append('../')
 
 
-def prepare_post_data():
-    post = pd.read_csv(data_file_path('clean/pprr_post_2020_11_06.csv'))
-    return post[post.agency == 'new orleans pd']
+def prepare_noso_post_data():
+    noso_post = pd.read_csv(data_file_path('clean/pprr_post_2020_11_06.csv'))
+    return noso_post[noso_post.agency == 'orleans parish so']
 
+
+def prepare_nopd_post_data():
+    nopd_post = pd.read_csv(data_file_path('clean/pprr_post_2020_11_06.csv'))
+    return nopd_post[nopd_post.agency == 'new orleans pd']
+
+
+def match_cprr_and_nopd_post(cprr, nopd_post):
+    dfa = cprr[['uid', 'first_name', 'last_name']].drop_duplicates(subset=['uid'])\
+        .set_index('uid', drop=True)
+    dfa.loc[:, 'fc'] = dfa.first_name.fillna('').map(lambda x: x[:1])
+
+    dfb = nopd_post[['uid', 'first_name', 'last_name']].drop_duplicates(subset=['uid'])\
+        .set_index('uid', drop=True)
+    dfb.loc[:, 'fc'] = dfb.first_name.fillna('').map(lambda x: x[:1])
+
+    matcher = ThresholdMatcher(ColumnsIndex(['fc']), {
+        'first_name': JaroWinklerSimilarity(),
+        'last_name': JaroWinklerSimilarity()
+    }, dfa, dfb)
+    decision = 1
+    matcher.save_pairs_to_excel(data_file_path(
+        "match/new_orleans_da_v_post_nopd_pprr_2020_11_06.xlsx"), decision)
+    matches = matcher.get_index_pairs_within_thresholds(decision)
+    match_dict = dict(matches)
+
+    cprr.loc[:, 'uid'] = cprr.uid.map(lambda x: match_dict[x])
+    return cprr
+
+
+def match_cprr_and_noso_post(cprr, noso_post):
+    dfa = cprr[['uid', 'first_name', 'last_name']].drop_duplicates(subset=['uid'])\
+        .set_index('uid', drop=True)
+    dfa.loc[:, 'fc'] = dfa.first_name.fillna('').map(lambda x: x[:1])
+
+    dfb = noso_post[['uid', 'first_name', 'last_name']].drop_duplicates(subset=['uid'])\
+        .set_index('uid', drop=True)
+    dfb.loc[:, 'fc'] = dfb.first_name.fillna('').map(lambda x: x[:1])
+
+    matcher = ThresholdMatcher(ColumnsIndex(['fc']), {
+        'first_name': JaroWinklerSimilarity(),
+        'last_name': JaroWinklerSimilarity()
+    }, dfa, dfb)
+    decision = 1
+    matcher.save_pairs_to_excel(data_file_path(
+        "match/new_orleans_da_v_post_noso_pprr_2020_11_06.xlsx"), decision)
+    matches = matcher.get_index_pairs_within_thresholds(decision)
+    match_dict = dict(matches)
+
+    cprr.loc[:, 'uid'] = cprr.uid.map(lambda x: match_dict[x])
+    return cprr
 
 def match_cprr(cprr, pprr):
     dfa = cprr[['first_name', 'last_name', 'uid']]
@@ -25,7 +75,7 @@ def match_cprr(cprr, pprr):
     }, dfa, dfb)
     decision = 1
     matcher.save_pairs_to_excel(data_file_path(
-        'match/cprr_new_orleans_da_2021_v_pprr_new_orleans_pd_1946_2018.xlsx'), decision)
+        'match/new_orleans_da_cprr_2021_v_new_orleans_pd_1946_2018.xlsx'), decision)
     matches = matcher.get_index_pairs_within_thresholds(decision)
     match_dict = dict(matches)
 
@@ -33,51 +83,20 @@ def match_cprr(cprr, pprr):
     return cprr
 
 
-def match_cprr_and_post(pprr, post):
-    dfa = pprr[['uid', 'first_name', 'last_name']]
-    dfa.loc[:, 'fc'] = dfa.first_name.map(lambda x: x[:1])
-    dfa = dfa.drop_duplicates(subset=['uid']).set_index('uid')
-
-    dfb = post[['uid', 'first_name', 'last_name']]
-    dfb.loc[:, 'fc'] = dfb.first_name.map(lambda x: x[:1])
-    dfb = dfb.drop_duplicates(subset=['uid']).set_index('uid')
-
-    matcher = ThresholdMatcher(ColumnsIndex(['fc']), {
-        'last_name': JaroWinklerSimilarity(),
-        'first_name': JaroWinklerSimilarity(),
-    }, dfa, dfb)
-    decision = 1
-    matcher.save_pairs_to_excel(data_file_path(
-        'match/cprr_new_orleans_da_2021_v_post_pprr_2020_11_06.xlsx'), decision)
-    matches = matcher.get_index_pairs_within_thresholds(lower_bound=decision)
-    match_dict = dict(matches)
-
-    pprr.loc[:, 'uid'] = pprr.uid.map(lambda x: match_dict.get(x, x))
-    return pprr
-
-
 if __name__ == '__main__':
-    cprr = pd.read_csv(data_file_path(
-        'clean/cprr_new_orleans_da_2021.csv'))
+    cprr = pd.read_csv(data_file_path('clean/cprr_new_orleans_da_2021.csv'))
     pprr = pd.read_csv(data_file_path('clean/pprr_new_orleans_pd_1946_2018.csv'))
-    post = prepare_post_data()
-    cprr = match_cprr(cprr, pprr)
-    ensure_data_dir('match')
-    cprr.to_csv(data_file_path(
-        'match/cprr_new_orleans_da_2021.csv'), index=False)
-
-if __name__ == '__main__':
-    cprr = pd.read_csv(data_file_path(
-        'clean/cprr_st_tammany_so_2011_2021.csv'))
-    pprr = pd.read_csv(data_file_path('clean/pprr_st_tammany_so_2020.csv'))
-    post = prepare_post_data()
+    nopd_post = prepare_nopd_post_data()
+    noso_post = prepare_noso_post_data()
     cprr = match_cprr(cprr, pprr)
     post_event = pd.concat([
-        match_pprr_and_post(pprr, post),
-        match_cprr_and_post(cprr, post)
+        match_cprr_and_nopd_post(cprr, nopd_post),
+        match_cprr_and_noso_post(cprr, noso_post)
     ]).drop_duplicates(ignore_index=True)
     ensure_data_dir('match')
     cprr.to_csv(data_file_path(
-        'match/cprr_st_tammany_so_2011_2021.csv'), index=False)
+        'match/cprr_scott_pd_2020.csv'), index=False)
     post_event.to_csv(data_file_path(
-        'match/post_event_st_tammany_so_2020.csv'), index=False)
+        'match/post_event_new_orleans_da_2021.csv'), index=False)
+
+
