c6658848fcdacd19369b25024864926cb147f465
Khoi Pham <pckhoi@gmail.com>
2021-06-08 10:52:02 +0700

    Fix error in match script

--- match/scott_pd.py
+++ match/scott_pd.py
@@ -1,5 +1,6 @@
 from datamatch import ColumnsIndex, JaroWinklerSimilarity, ThresholdMatcher
 from lib.path import data_file_path, ensure_data_dir
+from lib.post import extract_events_from_post
 import pandas as pd
 import sys
 sys.path.append('../')
@@ -33,7 +34,7 @@ def match_cprr(cprr, pprr):
     return cprr
 
 
-def match_pprr_and_post(pprr, post):
+def extract_post_events(pprr, post):
     dfa = pprr[['uid', 'first_name', 'last_name']]
     dfa.loc[:, 'fc'] = dfa.first_name.map(lambda x: x[:1])
     dfa = dfa.drop_duplicates(subset=['uid']).set_index('uid')
@@ -48,47 +49,18 @@ def match_pprr_and_post(pprr, post):
     }, dfa, dfb)
     decision = 1
     matcher.save_pairs_to_excel(data_file_path(
-        'match/scott_pd_pprr_2021_v_post_pprr_2020_11_06.xlsx'), decision)
+        "match/scott_pd_pprr_2021_v_post_pprr_2020_11_06.xlsx"), decision)
     matches = matcher.get_index_pairs_within_thresholds(lower_bound=decision)
-    match_dict = dict(matches)
 
-    pprr.loc[:, 'uid'] = pprr.uid.map(lambda x: match_dict.get(x, x))
-    return pprr
-
-
-def match_cprr_and_post(cprr, post):
-    dfa = cprr[['uid', 'first_name', 'last_name']]
-    dfa.loc[:, 'fc'] = dfa.first_name.map(lambda x: x[:1])
-    dfa = dfa.drop_duplicates(subset=['uid']).set_index('uid')
-
-    dfb = post[['uid', 'first_name', 'last_name']]
-    dfb.loc[:, 'fc'] = dfb.first_name.map(lambda x: x[:1])
-    dfb = dfb.drop_duplicates(subset=['uid']).set_index('uid')
-
-    matcher = ThresholdMatcher(ColumnsIndex(['fc']), {
-        'last_name': JaroWinklerSimilarity(),
-        'first_name': JaroWinklerSimilarity(),
-    }, dfa, dfb)
-    decision = 1
-    matcher.save_pairs_to_excel(data_file_path(
-        'match/scott_pd_cprr_2020_v_post_pprr_2020_11_06.xlsx'), decision)
-    matches = matcher.get_index_pairs_within_thresholds(lower_bound=decision)
-    match_dict = dict(matches)
-
-    cprr.loc[:, 'uid'] = cprr.uid.map(lambda x: match_dict.get(x, x))
-    return cprr
+    return extract_events_from_post(post, matches, "Scott PD")
 
 
 if __name__ == '__main__':
-    cprr = pd.read_csv(data_file_path(
-        'clean/cprr_scott_pd_2020.csv'))
+    cprr = pd.read_csv(data_file_path('clean/cprr_scott_pd_2020.csv'))
     pprr = pd.read_csv(data_file_path('clean/pprr_scott_pd_2021.csv'))
     post = prepare_post_data()
     cprr = match_cprr(cprr, pprr)
-    post_event = pd.concat([
-        match_pprr_and_post(pprr, post),
-        match_cprr_and_post(cprr, post)
-    ]).drop_duplicates(ignore_index=True)
+    post_event = extract_post_events(pprr, post)
     ensure_data_dir('match')
     cprr.to_csv(data_file_path(
         'match/cprr_scott_pd_2020.csv'), index=False)
