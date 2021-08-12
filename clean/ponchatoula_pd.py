f68fee4a3532195c32000e23d52518925ff79d78
Khoi Pham <pckhoi@gmail.com>
2021-08-12 10:33:34 +0700

    Add raw input for ponchatoula cprr

--- clean/ponchatoula_pd.py
+++ clean/ponchatoula_pd.py
@@ -1,3 +1,4 @@
+from lib.standardize import standardize_from_lookup_table
 import sys
 
 import pandas as pd
@@ -19,7 +20,7 @@ def clean_department_desc(df):
     return df
 
 
-def clean():
+def clean_pprr():
     return pd.read_csv(data_file_path(
         'raw/ponchatoula_pd/ponchatoula_pd_pprr_2010_2020.csv'
     )).pipe(clean_column_names)\
@@ -43,8 +44,60 @@ def clean():
         .pipe(gen_uid, ['agency', 'employee_id'])
 
 
+def clean_allegation(df):
+    df.loc[:, 'allegation'] = df.allegation.str.lower().str.strip()\
+        .str.replace(r'\bcommens\b', 'comments', regex=True)\
+        .str.replace(r'\bunbeomcing\b', 'unbecoming', regex=True)
+    return standardize_from_lookup_table(df, 'allegation', [
+        ['taking photos at crime scene'],
+        ['missed city court'],
+        ['no show for duty', 'no show'],
+        ['manpower shortage on his shift'],
+        ['late reports'],
+        ['lack of productivity'],
+        ['using cell phone in traffic'],
+        ["lock out assist and scratched citizen's point of view"],
+        ['allowing unauthorized personnel into restricted area'],
+        ['alterted police report'],
+        ['failed to transmit via police department radio of request for back up'],
+        ['off duty in marked unit 19 parked in handicap area'],
+        ['advised shift supervisor that he needed to be off to attend to personal business then worked a paid detail during same shift hours'],  # noqa
+        ['unsafe driving of police department unit'],
+        ['discourtesy', 'discourteous to co-workers'],
+        ['improper comments to city hall employees'],
+        ['offensive language'],
+        ['insubordination'],
+        ['conduct unbecoming'],
+        # 'loss of police department property' 'lack of attention to radio calls'
+        # 'punctuality/availability for duty' 'handling domestic dispute'
+        # 'vehicle damage (light)' 'inattentive with detainee'
+        # 'inappropriate use of emergency lights' 'personal domestic incident'
+        # 'time sheet discrepancy' 'unnecessary restraint' 'unprofessionalism '
+        # 'chemeical spraying of bystanders' 'misconduct with witness'
+        # 'rights violation' 'rudeness/unprofessionalism' 'missed court'
+        # 'unnecessary use of siren and emerging lights'
+        # 'mistreated | excessive force' 'delayed report writing' 'lax supervision'
+        # 'careless with police department equipment'
+        # 'lax documentation on warrants' 'late returning from break'
+        # 'failure to clear fta on stopped motorist'
+        # 'improper maintenance of equipment'
+        # "improper securing of arrestee's personal effects"
+        # 'improper securing of evidence' 'failure to complete crash report'
+        # 'minor damage to equipment' 'discourtest'
+    ])
+
+
+def clean_cprr():
+    return pd.read_csv(data_file_path(
+        'raw/ponchatoula_pd/ponchatoula_cprr_2010_2020.csv'
+    )).pipe(clean_column_names)\
+        .rename(columns={
+            'charges': 'allegation'
+        }).pipe(clean_allegation)
+
+
 if __name__ == '__main__':
-    df = clean()
+    df = clean_pprr()
     ensure_data_dir('clean')
     df.to_csv(data_file_path(
         'clean/pprr_ponchatoula_pd_2010_2020.csv'
