5d4c6179be48f71f9cfa06081b90749cdc17c35c
ayyubibrahimi <ayyub.ibrahimi@gmail.com>
2021-09-11 11:51:38 -0500

    added clean, match and fuse scripts

--- clean/lake_charles_pd_cprr.py
+++ clean/lake_charles_pd_cprr.py
@@ -1,11 +1,11 @@
-from lib.uid import gen_uid
 import sys
 sys.path.append('../')
 import pandas as pd
 from lib.path import data_file_path, ensure_data_dir
-from lib.clean import clean_names
+from lib.clean import float_to_int_str
 from lib.columns import clean_column_names
 from lib.rows import duplicate_row
+from lib.uid import gen_uid
 import re
 
 
@@ -45,7 +45,8 @@ def consolidate_action_and_disposition(df):
     df.loc[:, 'action'] = df.action.str.cat(df.disposition, sep='|')\
         .str.replace(r'(not)? ?sustained ?|exonerated ?|unfounded ?|invalid complaint ?', '', regex=True)\
         .str.replace(r'^\|', '', regex=True)\
-        .str.replace(r'\|$', '', regex=True)
+        .str.replace(r'\|$', '', regex=True)\
+        .str.replace(r'(\d+) (\w+)', r'\1-\2', regex=True)
     return df
 
 
@@ -53,19 +54,65 @@ def clean_disposition(df):
     df.loc[:, 'disposition'] = df.disposition.str.lower().str.strip()\
         .str.replace('invalid complaint', '', regex=False)\
         .str.replace('suspended', '', regex=False)
-    return df 
+    return df
 
 
 def split_rows_with_multiple_officers(df):
     i = 0
-    for idx in df[df.last_name.str.contains(r"/|,")].index:
-        s = df.loc[idx + i, "last_name"]
+    for idx in df[df.name.str.contains(r"/|,")].index:
+        s = df.loc[idx + i, "name"]
         parts = re.split(r"\s*(?:/|,)\s*", s)
         df = duplicate_row(df, idx + i, len(parts))
         for j, name in enumerate(parts):
-            df.loc[idx + i + j, "last_name"] = name
+            df.loc[idx + i + j, "name"] = name
         i += len(parts) - 1
     return df
+
+
+def drop_rows_missing_disp_charges_and_action(df):
+    return df[~((df.disposition == '') & (df.charges == '') & (df.action == ''))]
+
+
+def assign_empty_first_name_column(df):
+    df.loc[:, 'first_name'] = ''
+    return df
+
+
+def review_first_names_from_post():
+    df = pd.read_csv(data_file_path('match/lake_charles_pd_cprr_2020_extracted_first_names.csv'))
+    df = df\
+        .pipe(clean_column_names)
+    return df
+
+
+def assign_first_names_from_post(df):
+    df.loc[:, 'name'] = df.name.str.lower().str.strip()\
+        .str.replace('torres', 'torres paul', regex=False)\
+        .str.replace('redd', 'redd jeffrey', regex=False)\
+        .str.replace('romero', 'romero mayo', regex=False)\
+        .str.replace('nevels', 'nevels harold', regex=False)\
+        .str.replace('manuel', 'manuel carlos', regex=False)\
+        .str.replace('morrow', 'morrow errel', regex=False)\
+        .str.replace('mccloskey', 'mccloskey john', regex=False)\
+        .str.replace('myers', 'myers hannah', regex=False)\
+        .str.replace('mccue', 'mccue eddie', regex=False)\
+        .str.replace('mills', 'mills logan', regex=False)\
+        .str.replace('dougay', 'dougay bennon', regex=False)\
+        .str.replace('saunier', 'saunier john', regex=False)\
+        .str.replace('ewing', 'ewing joshua', regex=False)\
+        .str.replace('johnson', 'johnson martin', regex=False)\
+        .str.replace('jackson', 'jackson princeton', regex=False)\
+        .str.replace('baccigalopi', 'baccigalopi dakota', regex=False)\
+        .str.replace('breaux', 'breaux keithen', regex=False)\
+        .str.replace('falcon', 'falcon bendy', regex=False)\
+        .str.replace('ford', 'ford raymond', regex=False)\
+        .str.replace('perkins', 'perkins carlton', regex=False)\
+        .str.replace('ponthieaux', 'ponthieaux wilbert', regex=False)\
+        .str.replace('markham', 'markham alan', regex=False)
+    names = df.name.str.extract(r'(\w+) ?(.+)?')
+    df.loc[:, 'last_name'] = names[0]
+    df.loc[:, 'first_name'] = names[1].fillna('')
+    return df.drop(columns='name')
 
 
 def assign_agency(df):
@@ -74,12 +121,11 @@ def assign_agency(df):
 
 
 def clean():
-    df = pd.read_csv(data_file_path('raw/lake_charles/lake_charles_pd_cprr_2020.csv'))
+    df = pd.read_csv(data_file_path('raw/lake_charles_pd/lake_charles_pd_cprr_2020.csv'))
     df = df\
         .pipe(clean_column_names)\
         .rename(columns={
-            'date_of_investigation': 'investigation_start_date',
-            'name': 'last_name'
+            'date_of_investigation': 'investigation_start_date'
         })\
         .pipe(clean_charges)\
         .pipe(split_rows_with_multiple_charges)\
@@ -87,15 +133,17 @@ def clean():
         .pipe(consolidate_action_and_disposition)\
         .pipe(clean_disposition)\
         .pipe(split_rows_with_multiple_officers)\
-        .pipe(clean_names, ['last_name'])\
+        .pipe(drop_rows_missing_disp_charges_and_action)\
+        .pipe(assign_empty_first_name_column)\
+        .pipe(assign_first_names_from_post)\
         .pipe(assign_agency)\
-        .pipe(gen_uid, ['last_name', 'agency'])\
-        .pipe(gen_uid, ['last_name', 'investigation_start_date', 'charges', 'action'], 'complaint_uid')\
-        .drop_duplicates(subset=('complaint_uid'))
-    return df 
+        .pipe(float_to_int_str, ['investigation_start_date'])\
+        .pipe(gen_uid, ['first_name', 'last_name', 'agency'])\
+        .pipe(gen_uid, ['first_name', 'last_name', 'investigation_start_date', 'charges', 'action'], 'complaint_uid')
+    return df
 
 
 if __name__ == '__main__':
     df = clean()
     ensure_data_dir('clean')
-    df.to_csv(data_file_path('clean/cprr_lake_charles_pd_2020.csv'))
+    df.to_csv(data_file_path('clean/cprr_lake_charles_pd_2020.csv'), index=False)
