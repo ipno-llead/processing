1f1a373990e5cc406c35dfea5a3f87ea1245f136
Khoi Pham <pckhoi@gmail.com>
2021-09-14 10:44:43 +0700

    Add scripts for Sterlington PD

--- fuse/all.py
+++ fuse/all.py
@@ -38,7 +38,8 @@ def fuse_personnel():
         pd.read_csv(data_file_path("fuse/per_lafayette_so.csv")),
         pd.read_csv(data_file_path("fuse/per_lafayette_pd.csv")),
         pd.read_csv(data_file_path("fuse/per_hammond_pd.csv")),
-        pd.read_csv(data_file_path("fuse/per_lake_charles_pd.csv"))
+        pd.read_csv(data_file_path("fuse/per_lake_charles_pd.csv")),
+        pd.read_csv(data_file_path("fuse/per_sterlington_pd.csv")),
     ])).sort_values('uid', ignore_index=True)
 
 
@@ -72,7 +73,8 @@ def fuse_event():
         pd.read_csv(data_file_path("fuse/event_lafayette_so.csv")),
         pd.read_csv(data_file_path("fuse/event_lafayette_pd.csv")),
         pd.read_csv(data_file_path("fuse/event_hammond_pd.csv")),
-        pd.read_csv(data_file_path("fuse/event_lake_charles_pd.csv"))
+        pd.read_csv(data_file_path("fuse/event_lake_charles_pd.csv")),
+        pd.read_csv(data_file_path("fuse/event_sterlington_pd.csv")),
     ])).sort_values(['agency', 'event_uid'], ignore_index=True)
 
 
@@ -98,7 +100,7 @@ def fuse_complaint():
         pd.read_csv(data_file_path("fuse/com_lafayette_pd.csv")),
         pd.read_csv(data_file_path("fuse/com_hammond_pd.csv")),
         pd.read_csv(data_file_path("fuse/com_ponchatoula_pd.csv")),
-        pd.read_csv(data_file_path("fuse/com_lake_charles_pd.csv"))
+        pd.read_csv(data_file_path("fuse/com_lake_charles_pd.csv")),
     ])).sort_values(['agency', 'data_production_year', 'tracking_number'], ignore_index=True)
 
 
