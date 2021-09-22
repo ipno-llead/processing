018dfed8c6d47a541252d0c05c8f06f9874c31f8
ayyubibrahimi <ayyub.ibrahimi@gmail.com>
2021-09-21 23:46:10 -0500

    fixed match for cprr

--- match/plaquemines_so.py
+++ match/plaquemines_so.py
@@ -11,12 +11,12 @@ def prepare_post_data():
     return post[post.agency == 'plaquemines par so']
 
 
-def match_cprr_post(cprr, post):
+def match_cprr_and_pprr(cprr, pprr):
     dfa = cprr[['uid', 'first_name', 'last_name']].drop_duplicates(subset=['uid'])\
         .set_index('uid', drop=True)
     dfa.loc[:, 'fc'] = dfa.first_name.fillna('').map(lambda x: x[:1])
 
-    dfb = post[['uid', 'first_name', 'last_name']].drop_duplicates(subset=['uid'])\
+    dfb = pprr[['uid', 'first_name', 'last_name']].drop_duplicates(subset=['uid'])\
         .set_index('uid', drop=True)
     dfb.loc[:, 'fc'] = dfb.first_name.fillna('').map(lambda x: x[:1])
 
@@ -24,9 +24,9 @@ def match_cprr_post(cprr, post):
         'first_name': JaroWinklerSimilarity(),
         'last_name': JaroWinklerSimilarity()
     }, dfa, dfb)
-    decision = 0.8
+    decision = 0.97
     matcher.save_pairs_to_excel(data_file_path(
-        "match/plaquemines_so_cprr_2019_v_post_pprr_2020_11_06.xlsx"), decision)
+        "match/plaquemines_so_cprr_2019_v__pprr_plaquemines_so_2018.xlsx"), decision)
     matches = matcher.get_index_pairs_within_thresholds(decision)
     match_dict = dict(matches)
 
@@ -58,7 +58,7 @@ if __name__ == '__main__':
     cprr = pd.read_csv(data_file_path('clean/cprr_plaquemines_so_2019.csv'))
     pprr = pd.read_csv(data_file_path('clean/pprr_plaquemines_so_2018.csv'))
     post = prepare_post_data()
-    cprr = match_cprr_post(cprr, post)
+    cprr = match_cprr_and_pprr(cprr, post)
     post_event = match_pprr_and_post(pprr, post)
     ensure_data_dir('match')
     cprr.to_csv(data_file_path(
