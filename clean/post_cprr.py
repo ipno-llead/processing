2cb69dbae04f1c10d253e7eeba3dc53be6110f3f
ayyubibrahimi <ayyub.ibrahimi@gmail.com>
2021-10-13 19:24:41 -0500

    added clean and match scripts

---
+++ clean/post_cprr.py
@@ -0,0 +1,54 @@
+import sys
+sys.path.append('../')
+import pandas as pd
+from lib.path import data_file_path
+from lib.columns import clean_column_names
+from lib.uid import gen_uid
+
+
+def split_name(df):
+    col_name = [col for col in df.columns if col.endswith('name')][0]
+    names = df[col_name].str.strip().str.lower()\
+        .str.replace('van tran', 'vantran', regex=False).str.replace("de' clouet", "de'clouet", regex=False)\
+        .str.replace(r'(\w+) \b(\w{2})$', r'\2 \1', regex=True).str.replace(r"\"", "", regex=True)\
+        .str.extract(r'^(\w+) ?(\w{3,})? ?(jr|sr)? (\w+-?\'?\w+)$')
+
+    df.loc[:, 'first_name'] = names[0]
+    df.loc[:, 'middle_name'] = names[1]\
+        .fillna('')
+    df.loc[:, 'suffix'] = names[2]\
+        .fillna('')
+    df.loc[:, 'last_name'] = names[3]
+    df.loc[:, 'last_name'] = df.last_name.str.cat(df.suffix, sep=' ')
+    return df.drop(columns=['name', 'suffix'])
+
+
+def clean_charges(df):
+    df.loc[:, 'charges'] = df.reason\
+        .str.replace(r'\/', '|', regex=True)\
+        .str.replace('-', ':', regex=False)
+    return df.drop(columns='reason')
+
+
+def assign_action(df):
+    df.loc[:, 'action'] = 'decertified'
+    return df
+
+
+def clean():
+    df = pd.read_csv(data_file_path('raw/post_council/post_decertifications_2016_2019.csv'))\
+        .pipe(clean_column_names)\
+        .rename(columns={
+            'date': 'decertification_date'
+        })\
+        .pipe(split_name)\
+        .pipe(clean_charges)\
+        .pipe(assign_action)\
+        .pipe(gen_uid, ['first_name', 'last_name', 'agency'])
+    return df
+
+
+if __name__ == '__main__':
+    df = clean()
+    df.to_csv(data_file_path(
+        'clean/cprr_post_2016_2019.csv'), index=False)
---
+++ match/post.py
@@ -0,0 +1,38 @@
+import sys
+sys.path.append('../')
+import pandas as pd
+from datamatch import JaroWinklerSimilarity, ThresholdMatcher, ColumnsIndex
+from lib.path import data_file_path
+
+
+def match_cprr_with_nopd(cprr, pprr):
+    dfa = cprr[['first_name', 'last_name', 'uid']]
+    dfa.loc[:, 'fc'] = dfa.first_name.map(lambda x: x[:1])
+    dfa = dfa.drop_duplicates(subset=['uid']).set_index('uid')
+
+    dfb = pprr[['first_name', 'last_name', 'uid']]
+    dfb.loc[:, 'fc'] = dfb.first_name.map(lambda x: x[:1])
+    dfb = dfb.drop_duplicates(subset=['uid']).set_index('uid')
+
+    matcher = ThresholdMatcher(ColumnsIndex(['fc']), {
+        'first_name': JaroWinklerSimilarity(),
+        'last_name': JaroWinklerSimilarity(),
+    }, dfa, dfb)
+
+    decision = .8
+    matches = matcher.save_pairs_to_excel(data_file_path(
+        'match/post_cprr_2016_2019_v_nopd_pprr.xlsx'), decision)
+    matcher = matcher.get_index_pairs_within_thresholds(lower_bound=decision)
+    match_dict = dict(matches)
+
+    cprr.loc['uid'] = cprr.uid.map(lambda x: match_dict.get(x, x))
+    return cprr
+
+
+if __name__ == '__main__':
+    pprr = pd.read_csv(data_file_path('clean/pprr_baton_rouge_so_2021.csv'))
+    cprr = pd.read_csv(data_file_path('clean/cprr_post_2016_2019.csv'))
+    cprr = match_cprr_with_nopd(cprr, pprr)
+    cprr.to_csv(data_file_path(
+        'match/cprr_post_2016_2019.csv'), index=False)
+
