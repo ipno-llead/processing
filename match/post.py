ad267a94b8416fd6a736059b3174110c586524cb
ayyubibrahimi <ayyub.ibrahimi@gmail.com>
2021-10-14 11:53:40 -0500

    added match script

--- match/post.py
+++ match/post.py
@@ -5,7 +5,7 @@ from datamatch import JaroWinklerSimilarity, ThresholdMatcher, ColumnsIndex
 from lib.path import data_file_path
 
 
-def match_cprr_with_nopd(cprr, pprr):
+def match_cprr_with_brso(cprr, pprr):
     dfa = cprr[['first_name', 'last_name', 'uid']]
     dfa.loc[:, 'fc'] = dfa.first_name.map(lambda x: x[:1])
     dfa = dfa.drop_duplicates(subset=['uid']).set_index('uid')
@@ -21,7 +21,7 @@ def match_cprr_with_nopd(cprr, pprr):
 
     decision = .8
     matches = matcher.save_pairs_to_excel(data_file_path(
-        'match/post_cprr_2016_2019_v_nopd_pprr.xlsx'), decision)
+        'match/post_cprr_2016_2019_v_brso_pprr_2021.xlsx'), decision)
     matcher = matcher.get_index_pairs_within_thresholds(lower_bound=decision)
     match_dict = dict(matches)
 
@@ -30,9 +30,8 @@ def match_cprr_with_nopd(cprr, pprr):
 
 
 if __name__ == '__main__':
-    pprr = pd.read_csv(data_file_path('clean/pprr_baton_rouge_so_2021.csv'))
+    pprr = pd.read_csv(data_file_path('clean/pprr_lafayette_pd_2010_2021.csv'))
     cprr = pd.read_csv(data_file_path('clean/cprr_post_2016_2019.csv'))
-    cprr = match_cprr_with_nopd(cprr, pprr)
+    cprr = match_cprr_with_brso(cprr, pprr)
     cprr.to_csv(data_file_path(
         'match/cprr_post_2016_2019.csv'), index=False)
-
