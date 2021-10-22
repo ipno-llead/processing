ebe51794a5911c50501c835b06f30a6a727bdcb9
ayyubibrahimi <ayyub.ibrahimi@gmail.com>
2021-10-22 12:33:36 -0500

    incorporated khoi's feedback

--- clean/plaquemines_so_vine_booking.py
+++ clean/plaquemines_so_vine_booking.py
@@ -2,32 +2,42 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from lib.columns import clean_column_names
+from lib.clean import standardize_desc_cols
 from lib.path import data_file_path
 
 
-url = 'http://www.plaquemines.lavns.org/roster.aspx'
-r = requests.get(url)
-soup = BeautifulSoup(r.text, 'html.parser')
+def scrape(url):
+    try:
+        inmates = []
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        for table_row in soup.select("tr"):
+            cells = table_row.findAll('td', {'align': 'left', 'valign': 'middle'})
+            if len(cells) > 0:
+                name = cells[0].text
+                dob = cells[1].text
+                race = cells[2].text
+                sex = cells[3].text
+                arrest_date = cells[4].text
+                inmate = {'name': name, 'birth_date': dob, 'race': race, 'sex': sex, 'arrest_date': arrest_date}
+                inmates.append(inmate)
+    except Exception as e:
+        return e
 
-inmates_dict = []
-for table_row in soup.select("tr"):
-    cells = table_row.findAll('td', {'align': 'left', 'valign': 'middle'})
-    if len(cells) > 0:
-        name = cells[0].text
-        dob = cells[1].text
-        race = cells[2].text
-        gender = cells[3].text
-        arrest_date = cells[4].text
-        inmate = {'name': name, 'birth_date': dob, 'race': race, 'gender': gender, 'arrest_date': arrest_date}
-        inmates_dict.append(inmate)
+    df = pd.DataFrame.from_records(inmates)
+    df.to_csv(data_file_path('scraped/plaquemines_so_booking_log_10_21_2021_to_10_26_2021.csv'), index=False)
 
 
 def clean():
-    df = pd.DataFrame.from_dict(inmates_dict)\
-        .pipe(clean_column_names)
+    df = pd.read_csv(data_file_path('scraped/plaquemines_so_booking_log_10_21_2021_to_10_26_2021.csv'))\
+        .pipe(clean_column_names)\
+        .drop(columns=['name', 'birth_date'])\
+        .pipe(standardize_desc_cols, ['race', 'sex', 'arrest_date'])
     return df
 
 
 if __name__ == '__main__':
+    url = 'http://www.plaquemines.lavns.org/roster.aspx'
+    df = scrape(url)
     df = clean()
-    df.to_csv(data_file_path('clean/plaquemines_so_booking_log_10_21_2021.csv'), index=False)
+    df.to_csv(data_file_path('clean/plaquemines_so_booking_log_10_21_2021_to_10_26_2021.csv'), index=False)
