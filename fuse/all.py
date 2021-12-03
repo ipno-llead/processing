72bc7fa262724928826f212e6a7d13f71b160478
ayyubibrahimi <ayyub.ibrahimi@gmail.com>
2021-12-03 12:36:54 -0600

    added clean, match and fuse scripts; added pprr_gonzales to fuse/all.py and wrglsync.yaml

--- fuse/all.py
+++ fuse/all.py
@@ -47,6 +47,7 @@ def fuse_personnel():
         pd.read_csv(data_file_path("fuse/per_central_csd.csv")),
         pd.read_csv(data_file_path("fuse/per_bossier_city_pd.csv")),
         pd.read_csv(data_file_path("fuse/per_baker_pd.csv")),
+         pd.read_csv(data_file_path("fuse/per_gonzales_pd.csv")),
     ])).sort_values('uid', ignore_index=True)
 
 
@@ -88,6 +89,7 @@ def fuse_event():
         pd.read_csv(data_file_path("fuse/event_central_csd.csv")),
         pd.read_csv(data_file_path("fuse/event_bossier_city_pd.csv")),
         pd.read_csv(data_file_path("fuse/event_baker_pd.csv")),
+        pd.read_csv(data_file_path("fuse/event_gonzales_pd.csv")),
     ])).sort_values(['agency', 'event_uid'], ignore_index=True)
 
 
