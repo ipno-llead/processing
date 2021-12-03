d2a0e077cae3c79a103ac4e4c0e270e47ae888b5
ayyubibrahimi <ayyub.ibrahimi@gmail.com>
2021-12-03 13:35:12 -0600

    added clean, match and fuse scripts; added cprr_abbeville to fuse/all.py and wrglsync.yaml

--- fuse/all.py
+++ fuse/all.py
@@ -47,6 +47,7 @@ def fuse_personnel():
         pd.read_csv(data_file_path("fuse/per_central_csd.csv")),
         pd.read_csv(data_file_path("fuse/per_bossier_city_pd.csv")),
         pd.read_csv(data_file_path("fuse/per_baker_pd.csv")),
+        pd.read_csv(data_file_path("fuse/per_abbeville_pd.csv")),
     ])).sort_values('uid', ignore_index=True)
 
 
@@ -88,6 +89,7 @@ def fuse_event():
         pd.read_csv(data_file_path("fuse/event_central_csd.csv")),
         pd.read_csv(data_file_path("fuse/event_bossier_city_pd.csv")),
         pd.read_csv(data_file_path("fuse/event_baker_pd.csv")),
+        pd.read_csv(data_file_path("fuse/event_abbeville_pd.csv")),
     ])).sort_values(['agency', 'event_uid'], ignore_index=True)
 
 
@@ -116,6 +118,7 @@ def fuse_allegation():
         pd.read_csv(data_file_path("fuse/com_lake_charles_pd.csv")),
         pd.read_csv(data_file_path("fuse/com_bossier_city_pd.csv")),
         pd.read_csv(data_file_path("fuse/com_baker_pd.csv")),
+        pd.read_csv(data_file_path("fuse/com_abbeville_pd.csv")),
     ])).sort_values(['agency', 'tracking_number'], ignore_index=True)
 
 
