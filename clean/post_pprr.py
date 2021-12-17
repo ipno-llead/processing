8442d64545c9af50685f620838f3139529a8a613
ayyubibrahimi <ayyub.ibrahimi@gmail.com>
2021-12-16 21:17:43 -0600

    continued cleaning post

--- clean/post_pprr.py
+++ clean/post_pprr.py
@@ -1,7 +1,7 @@
 from lib.columns import clean_column_names, set_values
 from lib.path import data_file_path, ensure_data_dir
 from lib.clean import (
-    clean_names, clean_dates, standardize_desc_cols
+    clean_names, clean_dates, names_to_title_case, standardize_desc_cols
 )
 from lib.uid import gen_uid
 import pandas as pd
@@ -10,12 +10,19 @@ sys.path.append("../")
 
 
 def standardize_agency(df):
-    df.loc[:, "agency"] = df.agency.str.strip()\
-        .str.lower().fillna("").str.replace(r"(\w)\.\s*(\w)\.", r"\1\2", regex=True)\
-        .str.replace(r'e\. baton rouge so', 'baton rouge so', regex=True)\
-        .str.replace(r'e\. jefferson levee pd', 'east jefferson levee pd', regex=True)\
-        .str.replace(r'^st ', 'st. ', regex=True)\
-        .str.replace(' parish', '', regex=False)
+    df.loc[:, "agency"] = df.agency.str.strip().fillna("").str.replace(r"(\w)\.\s*(\w)\.", r"\1\2", regex=True)\
+        .str.replace(r'E\. Baton Rouge So', 'Baton Rouge SO', regex=True)\
+        .str.replace(r'E\. Jefferson Levee Pd', 'East Jefferson Levee PD', regex=True)\
+        .str.replace(r'^St ', 'St. ', regex=True)\
+        .str.replace(' Parish', '', regex=False)\
+        .str.replace(r'Pd$', 'PD', regex=True)\
+        .str.replace(r'So$', 'SO', regex=True)\
+        .str.replace(r'Dept\.', 'Department', regex=True)\
+        .str.replace(r'Univ\. Pd - (.+)', r'\1 University PD', regex=True)\
+        .str.replace(r'^Lsu\b', 'LSU', regex=True)\
+        .str.replace(r'^Lsuhsc', 'LSUHSC', regex=True)\
+        .str.replace(r'^La\b', 'Louisiana', regex=True)\
+        .str.replace('Orleans DA Office', 'New Orleans DA', regex=False)
     return df
 
 
@@ -34,12 +41,13 @@ def clean():
     df.loc[:, 'data_production_year'] = '2020'
     df = df.dropna(0, "all", subset=["first_name"])
     df = df\
-        .pipe(standardize_agency)\
         .pipe(standardize_desc_cols, ["employment_status"])\
         .pipe(clean_dates, ["hire_date"])\
         .pipe(replace_impossible_dates)\
         .pipe(clean_dates, ['level_1_cert_date', 'last_pc_12_qualification_date'], expand=False)\
         .pipe(clean_names, ["first_name", "last_name"])\
+        .pipe(names_to_title_case, ['agency'])\
+        .pipe(standardize_agency)\
         .pipe(set_values, {
             'source_agency': 'POST'
         })\
