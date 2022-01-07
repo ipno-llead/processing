0a4e7d884f83e97a672d5527ccfd02f68412a003
ayyubibrahimi <ayyub.ibrahimi@gmail.com>
2022-01-07 09:57:07 -0600

    continued to work on test functions

--- lib/clean_test.py
+++ lib/clean_test.py
@@ -5,7 +5,7 @@ import pandas as pd
 from pandas.testing import assert_frame_equal
 
 from clean import remove_future_dates
-from lib.clean import float_to_int_str
+from lib.clean import canonicalize_names, float_to_int_str
 
 sys.path.append("./")
 
@@ -39,19 +39,66 @@ class RemoveFutureDates(unittest.TestCase):
         )
 
 
-class FltIntStr(unittest.TestCase):
-    def test_float_to_int_str(self):
-        data = {"dates": ["1994", "2005", "2011", "1955"]}
-        df = pd.DataFrame(data)
+# class FltIntStr(unittest.TestCase):
+#     def test_float_to_int_str(self):
+#         data = {"dates": ["1994", "2005", "2011", "1955"]}
+#         df = pd.DataFrame(data)
 
-        df.loc[:, "dates"] = df.dates.astype(float)
-        df = df.pipe(float_to_int_str, ["dates"])
+#         df.loc[:, "dates"] = df.dates.astype(float)
+#         df = df.pipe(float_to_int_str, ["dates"])
 
-        dfa = pd.DataFrame(data)
-        dfa.loc[:, "dates"] = dfa.dates.astype(str)
+#         dfa = pd.DataFrame(data)
+#         dfa.loc[:, "dates"] = dfa.dates.astype(str)
 
-        df = df[["dates"]]
+#         df = df[["dates"]]
 
-        dfa = dfa[["dates"]]
+#         dfa = dfa[["dates"]]
 
-        assert_frame_equal(df, dfa)
+#         assert_frame_equal(df, dfa)
+
+
+class CanonicalizeNames(unittest.TestCase):
+    def test_canonicalize_names(self):
+        # officers = {
+        #     "uid": [
+        #         ("1e65c0807675ee3f25f6a6bf25eb121b"),
+        #         ("db3d392b404754b2d4a127ca0922d2f8"),
+        #         ("db3d392b404754b2d4a127ca0922d2f8"),
+        #         ("db3d392b404754b2d4a1fwrwaf3r3223"),
+        #     ],
+        #     "first_name": [("patrick"), ("patrick"), ("mariscos"), ("jimbo")],
+        #     "last_name": [("star"), ("starr"), ("consome"), ("cruz")],
+        # }
+
+        # clusters = {
+        #     "uid": [
+        #         ("1e65c0807675ee3f25f6a6bf25eb121b"),
+        #         ("db3d392b404754b2d4a127ca0922d2f8"),
+        #     ],
+        #     "first_name": [("patrick"), ("patrick")],
+        #     "last_name": [("star"), ("starr")],
+        # }
+
+        officers = [
+            ("92d3d9c79a3eb44ece5c83e62e55b91d", "thomas", "paine"),
+            ("1e65c0807675ee3f25f6a6bf25eb121b", "patrick", "peterman"),
+            ("db3d392b404754b2d4a127ca0922d2f8", "patrick", "petermann"),
+        ]
+        clusters = [
+            ("1e65c0807675ee3f25f6a6bf25eb121b", "patrick", "peterman"),
+            ("db3d392b404754b2d4a127ca0922d2f8", "patrick", "petermann"),
+        ]
+
+        data = pd.DataFrame(officers, columns=["uid", "first_name", "last_name"])
+
+        dfa = canonicalize_names(data, clusters)
+
+        canonicalized_officers = [
+            ("92d3d9c79a3eb44ece5c83e62e55b91d", "thomas", "paine"),
+            ("1e65c0807675ee3f25f6a6bf25eb121b", "patrick", "petermann"),
+            ("db3d392b404754b2d4a127ca0922d2f8", "patrick", "petermann"),
+        ]
+
+        dfb = pd.DataFrame(canonicalized_officers, columns=["uid", "first_name", "last_name"])
+
+        assert_frame_equal(dfa, dfb)
