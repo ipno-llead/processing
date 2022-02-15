90cdb8110a4ac34c8dc0b1ce9bad4faff728dfc1
ayyubibrahimi <ayyub.ibrahimi@gmail.com>
2022-02-15 16:35:31 -0600

    deleted reundant functions from fuse/rayne_pd

--- fuse/rayne_pd.py
+++ fuse/rayne_pd.py
@@ -1,14 +1,13 @@
 import sys
-
+sys.path.append('../')
 import pandas as pd
 from lib.path import data_file_path
 from lib import events
-from lib.columns import rearrange_allegation_columns
+from lib.columns import rearrange_allegation_columns, rearrange_event_columns
 from lib.personnel import fuse_personnel
-from lib.post import load_for_agency
 
 
-def fuse_events(cprr, post, pprr):
+def fuse_events(cprr, pprr):
     builder = events.Builder()
     builder.extract_events(
         cprr,
@@ -25,24 +24,6 @@ def fuse_events(cprr, post, pprr):
         ["uid", "allegation_uid"],
     )
     builder.extract_events(
-        post,
-        {
-            events.OFFICER_LEVEL_1_CERT: {
-                "prefix": "level_1_cert",
-                "parse_date": "%Y-%m-%d",
-                "keep": ["uid", "agency", "employment_status"],
-            },
-            events.OFFICER_HIRE: {
-                "prefix": "hire",
-                "keep": ["uid", "agency", "employment_status"],
-            },
-            events.OFFICER_PC_12_QUALIFICATION: {
-                "prefix": "last_pc_12_qualification",
-                "parse_date": "%Y-%m-%d",
-                "keep": ["uid", "agency", "employment_status"],
-            },
-        },
-    builder.extract_events(
         pprr,
         {
             events.OFFICER_HIRE: {
@@ -54,22 +35,18 @@ def fuse_events(cprr, post, pprr):
                 "keep": ["uid", "agency", "rank_desc", "left_reason"],
             },
         },
+        ["uid"]
     )
     return builder.to_frame()
 
 
 if __name__ == "__main__":
     cprr = pd.read_csv(data_file_path("clean/cprr_rayne_pd_2019_2020.csv"))
-    agency = cprr.agency[0]
-    post = load_for_agency("clean/pprr_post_2020_11_06.csv", agency)
-    per = fuse_personnel(cprr, post)
-    com = rearrange_allegation_columns(cprr)
-    event = fuse_events(cprr, post)
-    event.to_csv(data_file_path("fuse/event_rayne_pd.csv"), index=False)
-    com.to_csv(data_file_path("fuse/com_rayne_pd.csv"), index=False)
-    per.to_csv(data_file_path("fuse/per_rayne_pd.csv"), index=False)
     pprr = pd.read_csv(data_file_path("clean/pprr_rayne_pd_2010_2020.csv"))
     post_event = pd.read_csv(data_file_path("match/post_event_rayne_pd_2020_11_06.csv"))
-    events_df = rearrange_event_columns(pd.concat([post_event, fuse_events(pprr)]))
-    fuse_personnel(pprr).to_csv(data_file_path("fuse/per_rayne_pd.csv"), index=False)
+    per = fuse_personnel(cprr, pprr)
+    com = rearrange_allegation_columns(cprr)
+    events_df = rearrange_event_columns(pd.concat([post_event, fuse_events(cprr, pprr)]))
+    per.to_csv(data_file_path("fuse/per_rayne_pd.csv"), index=False)
+    com.to_csv(data_file_path("fuse/com_rayne_pd.csv"), index=False)
     events_df.to_csv(data_file_path("fuse/event_rayne_pd.csv"), index=False)
