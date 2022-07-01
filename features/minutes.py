94117a46632fd99208a0d1c50894b8cf82709de7
Khoi Pham <pckhoi@gmail.com>
2022-07-01 18:58:18 +0700

    feat: refactor minutes features extraction

--- features/minutes.py
+++ features/minutes.py
@@ -1,94 +1,5 @@
-import operator
-import re
-from functools import reduce
-
 import deba
 import pandas as pd
-
-REGEXES_MAP = {
-    "westwego": {
-        "mtg": [r"^The City of Westwego Municipal"],
-        "hrg": [r"^WESTWEGO MUNICIPAL FIRE AND POLICE", r"^Hearing of Appeal"],
-    },
-    "east_baton_rouge": {
-        "mtg": [
-            r"^MUNICIPAL FIRE (AND|&) POLICE",
-            r"CIVIL SERVICE BOARD$",
-            r"MINUTES OF THE MEETING",
-            r"COUNCIL CHAMBERS",
-        ]
-    },
-    "louisiana_state": {
-        "mtg": [
-            r"^MINUTES$",
-            r"STATE POLICE COMMISSION",
-            r"LOUISIANA STATE POLICE COMMISSION",
-            r"GENERAL BUSINESS MEETING",
-            r"MINUTES/ACTIONS",
-        ]
-    },
-    "vivian": {"mtg": [r"TOWN HALL MEETING", r"COUNCIL MEETING$", r"^MINUTES OF"]},
-    "mandeville": {
-        "mtg": [
-            r"^City of Mandeville$",
-            r"^Minutes of Meeting$",
-            r"^Municipal Police Employees",
-            r"Civil Service Board$",
-            r"^MUNICIPAL POLICE EMPLOYEES",
-        ]
-    },
-    "kenner": {
-        "mtg": [r"^MINUTES OF THE KENNER", r"^MEETING MINUTES$"],
-        "hrg": [r"^HEARING OF APPEAL$"],
-        "agd": [r"^AGENDA$"],
-    },
-    "addis": {
-        "mtg": [
-            r"TOWN OF ADDIS",
-            r"MINUTES",
-            r"The regular meeting of the Mayor and Town Council",
-            r"Town of Addis Minutes",
-        ]
-    },
-    "orleans": {
-        "mtg": [
-            r"^CIVIL SERVICE",
-            r"SERVICE COMMISSION$",
-            r"REGULAR MONTHLY MEETING",
-            r"SPECIAL MEETING",
-        ]
-    },
-    "sulphur": {"agd": [r"AGENDA"], "hrg": [r"Special Meeting .+ Appeal"]},
-    "broussard": {
-        "mtg": [r"^BROUSSARD MUNICIPAL FIRE", r"FIRE AND POLICE CIVIL SERVICE BOARD$"]
-    },
-    "carencro": {
-        "mtg": [
-            r"^Carencro Municipal Fire",
-            r"Fire and Police Civil Service Board$",
-            r"^Meeting held on",
-        ]
-    },
-    "harahan": {
-        "mtg": [r"^Harahan Municipal Fire", r"Fire & Police Civil Service Board$"]
-    },
-    "lake_charles": {
-        "agd": [r"^Notice$", r"Board will meet"],
-        "mtg": [
-            r"Board met",
-            r"^REGULAR MEETING MINUTES",
-            r"^LAKE CHARLES MUNICIPAL",
-            r"FIRE AND POLICE CIVIL SERVICE BOARD$",
-        ],
-    },
-    "youngsville": {"mtg": [r"^MINUTES OF THE"]},
-    "shreveport": {"mtg": [r"MINUTES"]},
-    "iberia": {"mtg": [r"^MINUTES OF"]},
-    "slidell": {
-        "agd": [r"^MEETING AGENDA$"],
-        "mtg": [r"^Board Members (Present|Absent)"],
-    },
-}
 
 
 def only_minutes(df: pd.DataFrame):
@@ -106,97 +17,183 @@ def split_lines(df: pd.DataFrame):
     df.loc[:, "text"] = df.text.str.strip().str.replace(r"\s+", " ", regex=True)
     df = df[df.text != ""].reset_index(drop=True)
     df.loc[:, "lineno"] = df.groupby(["fileid", "pageno"]).cumcount()
+    df.loc[:, "maxline"] = df.lineno.mask(
+        df.duplicated(["fileid", "pageno"], keep="last")
+    ).bfill()
     return df
 
 
-def extract_page_features(lines: pd.DataFrame):
-    page_fts = lines[["fileid", "pageno", "lineno", "text"]].set_index(
-        ["fileid", "pageno", "lineno"], drop=False
-    )
-    page_fts.loc[:, "hd"] = page_fts.lineno <= 3
-    linenos = (
-        lines[["fileid", "pageno", "lineno"]]
-        .groupby(["fileid", "pageno"])
-        .max()
-        .join(page_fts.lineno.to_frame(), how="right", lsuffix="_max")
-    )
-    page_fts.loc[:, "ft"] = linenos.lineno == linenos.lineno_max
-
-    re_map = {
-        "frontpage": [re.compile(s) for s in [r"page 1$", r"^1$"]],
-        "contpage": [
-            re.compile(s) for s in [r"page ([2-9]|1[0-9])", r"^([2-9]|1[0-9])$"]
-        ],
-    }
-
-    def text_match_one_of(row: pd.DataFrame, patterns):
-        if pd.isna(row.text):
-            return False
-        return reduce(
-            operator.or_,
-            [pattern.match(row.text) is not None for pattern in patterns],
-            False,
+def extract_pagetype(df: pd.DataFrame):
+    pagetypes = {
+        "meeting": (
+            (df.region == "westwego")
+            & (df.text.str.match(r"^The City of Westwego Municipal"))
         )
-
-    for feat, patterns in re_map.items():
-        page_fts.loc[:, feat] = page_fts.apply(
-            text_match_one_of, axis=1, args=(patterns,)
-        )
-
-    page_fts = (
-        page_fts.reset_index(drop=True)[["fileid", "pageno"] + list(re_map.keys())]
-        .groupby(["fileid", "pageno"])
-        .max()
-    )
-
-    return page_fts
-
-
-def extract_doctype_feats(lines: pd.DataFrame):
-    feats = lines.loc[
-        lines.lineno <= 8, ["region", "fileid", "pageno", "lineno", "text"]
-    ].set_index(["fileid", "pageno", "lineno"])
-
-    def match(group: pd.DataFrame):
-        region = group.region[0]
-        if region not in REGEXES_MAP:
-            return group
-        for feat, patterns in REGEXES_MAP[region].items():
-            group.loc[:, feat] = reduce(
-                operator.or_,
-                [group.text.str.match(pattern) for pattern in patterns],
-                False,
+        | (
+            (df.region == "east_baton_rouge")
+            & (
+                df.text.str.match(r"^MUNICIPAL FIRE (AND|&) POLICE")
+                | df.text.str.match(r"CIVIL SERVICE BOARD$")
+                | df.text.str.match(r"MINUTES OF THE MEETING")
+                | df.text.str.match(r"COUNCIL CHAMBERS")
             )
-        return group
+        )
+        | (
+            (df.region == "louisiana_state")
+            & (
+                df.text.str.match(r"^MINUTES$")
+                | df.text.str.match(r"STATE POLICE COMMISSION")
+                | df.text.str.match(r"LOUISIANA STATE POLICE COMMISSION")
+                | df.text.str.match(r"GENERAL BUSINESS MEETING")
+                | df.text.str.match(r"MINUTES/ACTIONS")
+            )
+        )
+        | (
+            (df.region == "vivian")
+            & (
+                df.text.str.match(r"TOWN HALL MEETING")
+                | df.text.str.match(r"COUNCIL MEETING$")
+                | df.text.str.match(r"^MINUTES OF")
+            )
+        )
+        | (
+            (df.region == "mandeville")
+            & (
+                df.text.str.match(r"^City of Mandeville$")
+                | df.text.str.match(r"^Minutes of Meeting$")
+                | df.text.str.match(r"^Municipal Police Employees")
+                | df.text.str.match(r"Civil Service Board$")
+                | df.text.str.match(r"^MUNICIPAL POLICE EMPLOYEES")
+            )
+        )
+        | (
+            (df.region == "kenner")
+            & (
+                df.text.str.match(r"^MINUTES OF THE KENNER")
+                | df.text.str.match(r"^MEETING MINUTES$")
+            )
+        )
+        | (
+            (df.region == "addis")
+            & (
+                df.text.str.match(r"TOWN OF ADDIS")
+                | df.text.str.match(r"MINUTES")
+                | df.text.str.match(
+                    r"The regular meeting of the Mayor and Town Council"
+                )
+                | df.text.str.match(r"Town of Addis Minutes")
+            )
+        )
+        | (
+            (df.region == "orleans")
+            & (
+                df.text.str.match(r"^CIVIL SERVICE")
+                | df.text.str.match(r"SERVICE COMMISSION$")
+                | df.text.str.match(r"REGULAR MONTHLY MEETING")
+                | df.text.str.match(r"SPECIAL MEETING")
+            )
+        )
+        | (
+            (df.region == "broussard")
+            & (
+                df.text.str.match(r"^BROUSSARD MUNICIPAL FIRE")
+                | df.text.str.match(r"FIRE AND POLICE CIVIL SERVICE BOARD$")
+            )
+        )
+        | (
+            (df.region == "carencro")
+            & (
+                df.text.str.match(r"^Carencro Municipal Fire")
+                | df.text.str.match(r"Fire and Police Civil Service Board$")
+                | df.text.str.match(r"^Meeting held on")
+            )
+        )
+        | (
+            (df.region == "harahan")
+            & (
+                df.text.str.match(r"^Harahan Municipal Fire")
+                | df.text.str.match(r"Fire & Police Civil Service Board$")
+            )
+        )
+        | (
+            (df.region == "lake_charles")
+            & (
+                df.text.str.match(r"Board met")
+                | df.text.str.match(r"^REGULAR MEETING MINUTES")
+                | df.text.str.match(r"^LAKE CHARLES MUNICIPAL")
+                | df.text.str.match(r"FIRE AND POLICE CIVIL SERVICE BOARD$")
+            )
+        )
+        | ((df.region == "youngsville") & df.text.str.match(r"^MINUTES OF THE"))
+        | ((df.region == "shreveport") & df.text.str.match(r"MINUTES"))
+        | ((df.region == "iberia") & df.text.str.match(r"^MINUTES OF"))
+        | (
+            (df.region == "slidell")
+            & df.text.str.match(r"^Board Members (Present|Absent)")
+        ),
+        "hearing": (
+            (df.region == "westwego")
+            & (
+                df.text.str.match(r"^WESTWEGO MUNICIPAL FIRE AND POLICE")
+                | df.text.str.match(r"^Hearing of Appeal")
+            )
+        )
+        | ((df.region == "kenner") & df.text.str.match(r"^HEARING OF APPEAL$"))
+        | ((df.region == "sulphur") & df.text.str.match(r"Special Meeting .+ Appeal")),
+        "agenda": ((df.region == "kenner") & df.text.str.match(r"^AGENDA$"))
+        | ((df.region == "sulphur") & df.text.str.match(r"AGENDA"))
+        | (
+            (df.region == "lake_charles")
+            & (df.text.str.match(r"^Notice$") | df.text.str.match(r"Board will meet"))
+        )
+        | ((df.region == "slidell") & df.text.str.match(r"^MEETING AGENDA$")),
+    }
+    df.loc[:, "pagetype"] = ""
+    for pt, index in pagetypes.items():
+        df.loc[index, "pagetype"] = pt
+    return df
 
-    feats = feats.groupby("region").apply(match)
-    feat_cols = ["mtg", "hrg", "agd"]
-    feats.loc[:, feat_cols] = feats[feat_cols].fillna(False)
-    return feats.reset_index(drop=False).groupby(["fileid", "pageno"])[feat_cols].max()
+
+def extract_contpage(df: pd.DataFrame):
+    df.loc[:, "contpage"] = False
+    df.loc[:, "frontpage"] = False
+    df.loc[
+        ((df.lineno == 0) | (df.lineno == df.maxline))
+        & (
+            df.text.str.match(r"page ([2-9]|1[0-9])")
+            | df.text.str.match(r"^([2-9]|1[0-9])$")
+        ),
+        "contpage",
+    ] = True
+    df.loc[
+        ((df.lineno == 0) | (df.lineno == df.maxline))
+        & (df.text.str.match(r"page 1$") | df.text.str.match(r"^1$")),
+        "frontpage",
+    ] = True
+    return df
 
 
-def get_header_features(df: pd.DataFrame):
-    lines = split_lines(df)
-    doctype_feats = extract_doctype_feats(lines)
-    page_feats = extract_page_features(lines)
-
-    return (
-        df.set_index(["fileid", "pageno"])
-        .join(doctype_feats)
-        .join(page_feats)
-        .reset_index()
-    )
+def aggregate_feats(df: pd.DataFrame):
+    grouped = df.groupby(["fileid", "pageno"])
+    for feat in ["pagetype", "contpage", "frontpage"]:
+        df.loc[:, feat] = grouped[feat].transform(lambda x: x.max())
+    return df[
+        ["fileid", "pageno", "region", "pagetype", "frontpage", "contpage"]
+    ].drop_duplicates()
 
 
-def extract_features():
+def minutes_features():
     return (
         pd.read_csv(deba.data("ocr/minutes_pdfs.csv"))
         .pipe(only_minutes)
         .pipe(discard_empty_pages)
-        .pipe(get_header_features)
+        .pipe(split_lines)
+        .pipe(extract_pagetype)
+        .pipe(extract_contpage)
+        .pipe(aggregate_feats)
     )
 
 
 if __name__ == "__main__":
-    df = extract_features()
+    df = minutes_features()
     df.to_csv(deba.data("features/minutes.csv"), index=False)
