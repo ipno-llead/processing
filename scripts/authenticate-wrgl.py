0aaf432fc19b36ff038ae83d47d8d416449309a4
Khoi Pham <pckhoi@gmail.com>
2022-12-15 17:54:14 +0700

    feat: update wrgl repo origin

---
+++ scripts/authenticate-wrgl.py
@@ -0,0 +1,16 @@
+import argparse
+
+from wrgl import Repository
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog="authenticate-wrgl",
+        description="exchanges Keycloak credentials for access token",
+    )
+    parser.add_argument("repo_uri")
+    parser.add_argument("client_id")
+    parser.add_argument("client_secret")
+    args = parser.parse_args()
+    repo = Repository(args.repo_uri, args.client_id, args.client_secret)
+    rpt = repo.authenticate()
+    print(rpt)
