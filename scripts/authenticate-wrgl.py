66c44d400a78402ea63b2bfd943f03297ee81ba4
Khoi Pham <pckhoi@gmail.com>
2022-12-15 18:38:15 +0700

    feat: print repo_uri and client_id during authentication

--- scripts/authenticate-wrgl.py
+++ scripts/authenticate-wrgl.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 import argparse
+import sys
 
 from wrgl import Repository
+
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -12,6 +18,7 @@ if __name__ == "__main__":
     parser.add_argument("client_id")
     parser.add_argument("client_secret")
     args = parser.parse_args()
+    eprint(f"authorizing access to {args.repo_uri} as {args.client_id}")
     repo = Repository(args.repo_uri, args.client_id, args.client_secret)
     rpt = repo.authenticate()
     print(rpt)
