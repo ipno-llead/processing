47d2547f423567af202f29122c98d4e073449406
Ayyub I <ayyub.ibrahimi@gmail.com>
2023-03-28 19:51:58 -0500

    fused brady per tables into master per table

--- fuse/all.py
+++ fuse/all.py
@@ -13,12 +13,14 @@ if __name__ == "__main__":
     agencies = pd.read_csv(deba.data("fuse_agency/agency_reference_list.csv"))
     documents = pd.read_csv(deba.data("clean/documents.csv"))
     post = pd.read_csv(deba.data("match_history/post_officer_history.csv"))
+    brady_df = pd.read_csv(deba.data("fuse_agency/brady.csv"))
 
     allegation_df = allegation_df[allegation_df["uid"].isin(per_df["uid"])]
     uof_df = uof_df[uof_df["uid"].isin(per_df["uid"])]
     event_df = event_df[event_df["uid"].isin(per_df["uid"])]
     sas_df = sas_df[sas_df["uid"].isin(per_df["uid"])]
     appeals_df = appeals_df[appeals_df["uid"].isin(per_df["uid"])]
+    brady_df = brady_df[brady_df["uid"].isin(per_df["uid"])]
 
     event_df = event_df[~((event_df.agency.fillna("") == ""))]
 
@@ -32,3 +34,4 @@ if __name__ == "__main__":
     citizens.to_csv(deba.data("fuse/citizens.csv"), index=False)
     agencies.to_csv(deba.data("fuse/agency_reference_list.csv"), index=False)
     documents.to_csv(deba.data("fuse/documents.csv"), index=False)
+    brady_df.to_csv(deba.data("fuse/brady.csv"), index=False)
