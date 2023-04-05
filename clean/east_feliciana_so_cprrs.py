b7810f278327dc844526111553491255b92f4673
Ayyub I <ayyub.ibrahimi@gmail.com>
2023-04-05 12:54:38 -0500

    re-added file to dvc

--- clean/east_feliciana_so_cprrs.py
+++ clean/east_feliciana_so_cprrs.py
@@ -5,13 +5,13 @@ import deba
 
 
 def clean():
-    df = (pd.read_csv(deba.data("raw/east_feliciana_so/cprr_east_feliciana_so_2016_2023_byhand.csv"), encoding="ISO-8859-1")
+    return (
+        pd.read_csv(deba.data("raw/east_feliciana_so/cprr_east_feliciana_so_2016_2023_byhand.csv"), encoding="ISO-8859-1")
           .pipe(clean_column_names)
           .pipe(set_values, {"agency": "east-feliciana-so"})
           .pipe(gen_uid, ["first_name", "last_name", "agency"])
           .pipe(gen_uid, ["allegation_desc", "disposition", "uid"], "allegation_uid")
-    )
-    return df
+        )
 
 
 if __name__ == "__main__":
