9c464b1b048f89d60533d11ca525255256361c65
Hung Nguyen <hung.d.nguyen@eastagile.com>
2023-04-06 16:43:46 +0700

    Copied the work from the fork repo to the original repo in order to utilize github secrets

---
+++ data-validator/citizens_importer.py
@@ -0,0 +1,105 @@
+import os
+import pandas as pd
+from slack_sdk import WebClient
+# from wrgl import Repository
+
+
+# def __retrieve_citizen_frm_wrgl_data(branch=None):
+#     repo = Repository("https://wrgl.llead.co/", None)
+
+#     original_commit = repo.get_branch("citizens")
+
+#     columns = original_commit.table.columns
+#     if not set(CITIZEN_COLS).issubset(set(columns)):
+#         raise Exception('BE citizens columns are not recognized in the current commit')
+
+#     all_rows = list(repo.get_blocks("heads/citizens"))
+#     df = pd.DataFrame(all_rows)
+#     df.columns = df.iloc[0]
+#     df = df.iloc[1:].reset_index(drop=True)
+
+#     df.to_csv('citizens.csv', index=False)
+
+
+def __build_citizen_rel(db_con, citizens_df, citizens_cols):
+    client = WebClient(os.environ.get('SLACK_BOT_TOKEN'))
+
+    print('Building relationship between agency and citizens')
+    agency_df = pd.read_sql(
+        'SELECT id, agency_slug FROM departments_department',
+        con=db_con
+    )
+    agency_df.columns = ['department_id', 'agency']
+
+    result = pd.merge(citizens_df, agency_df, how='left', on='agency')
+
+    print('Check agency id after merged')
+    null_department_data = result[result['department_id'].isnull()]
+    if len(null_department_data) > 0:
+        null_department_data.drop_duplicates(subset=['agency'], inplace=True)
+        null_department_data.to_csv(
+            'null_agency_of_citizen.csv',
+            columns=['agency'],
+            index=False,
+            header=False
+        )
+
+        client.files_upload(
+            channels=os.environ.get('SLACK_CHANNEL'),
+            title="Null agency of citizen",
+            file="./null_agency_of_citizen.csv",
+            initial_comment="The following file provides a list of agency in citizens that cannot map to departments:",
+        )
+
+        raise Exception('Cannot map agency to citizen')
+
+    print('Build relationship between complaints and citizens')
+    complaints_df = pd.read_sql(
+        'SELECT id, allegation_uid FROM complaints_complaint',
+        con=db_con
+    )
+    complaints_df.columns = ['complaint_id', 'allegation_uid']
+
+    result = pd.merge(result, complaints_df, how='left', on='allegation_uid')
+
+    print('Build relationship between uof and citizens')
+    uof_df = pd.read_sql(
+        'SELECT id, uof_uid FROM use_of_forces_useofforce',
+        con=db_con
+    )
+    uof_df.columns = ['use_of_force_id', 'uof_uid']
+
+    result = pd.merge(result, uof_df, how='left', on='uof_uid')
+
+    result = result.loc[:, citizens_cols + ['department_id', 'complaint_id', 'use_of_force_id']]
+    result = result.astype({
+        'department_id': int,
+        'complaint_id': pd.Int64Dtype(),
+        'use_of_force_id': pd.Int64Dtype(),
+        'citizen_age': pd.Int64Dtype()
+    })
+    result.to_csv('citizens.csv', index=False)
+
+
+def run(db_con, citizens_df, citizens_cols):
+    __build_citizen_rel(db_con, citizens_df, citizens_cols)
+
+    cursor = db_con.cursor()
+    cursor.copy_expert(
+        sql=f"""
+            COPY citizens_citizen(
+                {', '.join(citizens_cols + ['department_id', 'complaint_id', 'use_of_force_id'])}
+            )
+            FROM stdin WITH CSV HEADER
+            DELIMITER as ','
+        """,
+        file=open('citizens.csv', 'r'),
+    )
+    db_con.commit()
+    cursor.close()
+
+    count = pd.read_sql(
+        'SELECT COUNT(*) FROM citizens_citizen',
+        con=db_con
+    )
+    print('Number of records in citizen', count.iloc[0][0])
---
+++ data-validator/use_of_force_importer.py
@@ -0,0 +1,108 @@
+import os
+import pandas as pd
+from slack_sdk import WebClient
+# from wrgl import Repository
+
+
+# def __retrieve_uof_frm_wrgl_data(branch=None):
+#     repo = Repository("https://wrgl.llead.co/", None)
+
+#     original_commit = repo.get_branch("use-of-force")
+
+#     columns = original_commit.table.columns
+#     if not set(UOF_COLS).issubset(set(columns)):
+#         raise Exception('BE use-of-force columns are not recognized in the current commit')
+
+#     all_rows = list(repo.get_blocks("heads/use-of-force"))
+#     df = pd.DataFrame(all_rows)
+#     df.columns = df.iloc[0]
+#     df = df.iloc[1:].reset_index(drop=True)
+
+#     df.to_csv('uof.csv', index=False)
+
+
+def __build_uof_rel(db_con, uof_df, uof_cols):
+    client = WebClient(os.environ.get('SLACK_BOT_TOKEN'))
+
+    print('Check integrity between officers\' agency and uof agency')
+    officers_df = pd.read_sql(
+        'SELECT id, uid, agency FROM officers_officer',
+        con=db_con
+    )
+    officers_df.columns = ['officer_id', 'uid', 'officer_agency_slug']
+
+    uofor_df = pd.merge(uof_df, officers_df, how='left', on='uid')
+
+    print('Check officer id after merged')
+    null_officers_data = uofor_df[uofor_df['officer_id'].isnull()]
+    if len(null_officers_data) > 0:
+        null_officers_data.to_csv('null_officers_of_uof.csv', index=False)
+
+        client.files_upload(
+            channels=os.environ.get('SLACK_CHANNEL'),
+            title="Null officers of uof",
+            file="./null_officers_of_uof.csv",
+            initial_comment="The following file provides a list of personnels that cannot map to uof:",
+        )
+
+        raise Exception('Cannot map officer to uof')
+
+    uofor_df['agency_slug'] = uofor_df.apply(
+        lambda x: x['officer_agency_slug'] if pd.isnull(x['agency']) \
+                    else x['agency'],
+        axis=1
+    )
+
+    print('Check agency discrepancy')
+    diff_agency_data = uofor_df[uofor_df['officer_agency_slug'] != uofor_df['agency_slug']]
+    if len(diff_agency_data) > 0:
+        raise Exception('There are discrepancy between officer agency and uof agency')
+
+    agency_df = pd.read_sql(
+        'SELECT id, agency_slug FROM departments_department',
+        con=db_con
+    )
+    agency_df.columns = ['department_id', 'agency_slug']
+
+    result = pd.merge(uofor_df, agency_df, how='left', on='agency_slug')
+
+    print('Check agency id after merged')
+    null_department_data = result[result['department_id'].isnull()]
+    if len(null_department_data) > 0:
+        null_department_data.to_csv('null_agency_of_uof.csv', index=False)
+
+        client.files_upload(
+            channels=os.environ.get('SLACK_CHANNEL'),
+            title="Null agency of uof",
+            file="./null_agency_of_uof.csv",
+            initial_comment="The following file provides a list of agency that cannot map to uof:",
+        )
+
+        raise Exception('Cannot map agency to uof')
+
+    result = result.loc[:, uof_cols + ['officer_id', 'department_id']]
+    result.to_csv('uof.csv', index=False)
+
+
+def run(db_con, uof_df, uof_cols):
+    __build_uof_rel(db_con, uof_df, uof_cols)
+
+    cursor = db_con.cursor()
+    cursor.copy_expert(
+        sql=f"""
+            COPY use_of_forces_useofforce(
+                {', '.join(uof_cols + ['officer_id', 'department_id'])}
+            )
+            FROM stdin WITH CSV HEADER
+            DELIMITER as ','
+        """,
+        file=open('uof.csv', 'r'),
+    )
+    db_con.commit()
+    cursor.close()
+
+    count = pd.read_sql(
+        'SELECT COUNT(*) FROM use_of_forces_useofforce',
+        con=db_con
+    )
+    print('Number of records in useofforce', count.iloc[0][0])
