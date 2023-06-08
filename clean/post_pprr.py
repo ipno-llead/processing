b4b29202ec4152be490bfb9afae4e8e9bf9349fd
Ayyub I <ayyub.ibrahimi@gmail.com>
2023-06-08 12:53:55 -0500

    reverted to old post date format

--- clean/post_pprr.py
+++ clean/post_pprr.py
@@ -236,7 +236,6 @@ def clean20():
         df.pipe(clean_agency)
         .pipe(standardize_desc_cols, ["employment_status"])
         .pipe(clean_dates, ["hire_date"])
-        .pipe(fix_date_format)
         .pipe(replace_impossible_dates)
         .pipe(clean_names, ["first_name", "last_name"])
         .pipe(
