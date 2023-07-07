7424a59b324d2d9d91ad8b253f89cca7c45d8f8e
Ayyub I <ayyub.ibrahimi@gmail.com>
2023-07-07 18:54:16 -0500

    refactored cross agency script

--- fuse/cross_agency.py
+++ fuse/cross_agency.py
@@ -210,18 +210,14 @@ def create_person_table(clusters, personnel, personnel_event, post):
 
     # join uids with comma
     person_df.loc[:, "uids"] = person_df.uids.str.join(",")
-    person_df = person_df.drop_duplicates(subset=["canonical_uid", "uids"], keep="first")
 
     post_df = post[["history_id", "uid"]]
     post_df["canonical_uid"] = post_df["uid"]
 
     post_df = post_df.rename(columns={"history_id": "person_id", "uid": "uids"})
 
-    person_df = pd.concat([post_df, person_df])
-
-    post_uids = [x for x in post["history_id"]]
-
-    person_df = person_df[person_df.person_id.isin(post_uids)]
+    person_df = pd.concat([post_df, person_df],axis=0)
+    person_df = person_df.drop_duplicates(subset=["canonical_uid", "uids"], keep="first")
     return person_df[["person_id", "canonical_uid", "uids"]]
 
 
