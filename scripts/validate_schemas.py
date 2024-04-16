574e45ecec1bdad8be086bdb60cc53ebe4812080
Bao Dang <open-source@eastagile.com>
2024-04-16 11:06:55 +0700

    [c] Clean up WRGL

--- scripts/validate_schemas.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-validating_schemas_api = "https://api.llead.co/api/schemas/validate/"
-
-response = requests.post(validating_schemas_api)
-
-print("Schema validation finished!") if response.ok else print("Schema validation failed!")
