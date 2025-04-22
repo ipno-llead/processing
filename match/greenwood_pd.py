0163b80d2e0eeb3d0faf9239569fce8359fc0655
Esme Lee <esmelee@Esmes-MacBook-Air.local>
2025-04-22 11:04:55 -0500

    I think this is correct matching script, ?

--- match/greenwood_pd.py
+++ match/greenwood_pd.py
@@ -63,7 +63,7 @@ def match_pprr_and_post(pprr, post):
         dfa,
         dfb,
     )
-    decision = 0.96
+    decision = 1.0
     matcher.save_pairs_to_excel(
         deba.data("match/pprr_greenwood_1990_2001_v_post.xlsx"),
         decision,
