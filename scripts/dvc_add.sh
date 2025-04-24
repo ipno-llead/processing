29591fce6e25f8f71dc50d6399d450d1189619ae
Esme Lee <esmelee@Esmes-MacBook-Air.local>
2025-04-24 18:59:25 -0500

    we updated dvc_add.sh to work with dvc==3.58.0; updated process_data.yml to work with new dvc output format; updated greenwood_pd.dvc to include new csv for new pprr greenwood data 1990-2001

--- scripts/dvc_add.sh
+++ scripts/dvc_add.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 set -ex
 
+
 for folder in data/raw
 do
-    mkdir -p dvc/$folder || true
-    find $folder -type d -mindepth 1 -maxdepth 1 | xargs -I{} dvc add --file dvc/{}.dvc {}
+   mkdir -p dvc/$folder || true
+   # Find subdirectories and process each one
+   find $folder -type d -mindepth 1 -maxdepth 1 | while read dir; do
+       # Extract just the directory name without the path
+       dir_name=$(basename "$dir")
+       # Run dvc add from the directory where we want the .dvc file
+       (cd dvc/$folder/ && dvc add -f "../../../$dir")
+       # Rename the .dvc file to match the directory name
+       mv dvc/$folder/$(basename $dir).dvc dvc/$folder/$dir_name.dvc
+   done
 done
 
-dvc add --file raw_minutes.dvc data/raw_minutes
+
+# Add the raw_minutes directory
+(cd . && dvc add -f data/raw_minutes)
+mv data/raw_minutes.dvc raw_minutes.dvc
