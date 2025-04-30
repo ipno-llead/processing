e09f1a65bb21aea0deec380c4b44e40512dd32ed
Esme Lee <esmelee@Esmes-MacBook-Air.local>
2025-04-30 10:54:14 -0500

    troubleshooting OLDPD error: 'Check for integrity of officer agency and agency' -- dropped assignment column and stripped extra leading columns from levee cleaning script now trying make

--- clean/levee_pd_pprr.py
+++ clean/levee_pd_pprr.py
@@ -1,7 +1,7 @@
 import deba
 import pandas as pd
 from lib.columns import clean_column_names, set_values
-from lib.clean import clean_rank, split_names, clean_names, strip_leading_comma, standardize_desc_cols
+from lib.clean import clean_rank, split_names, clean_names, strip_leading_comma, standardize_desc_cols 
 from lib.uid import gen_uid
 
 
@@ -31,17 +31,29 @@ def strip_sex_and_race(df):
     df["sex"] = df["sex"].str.strip(" '\"")
     return df
 
+def clean_rank(df): 
+    df["rank"] = df["rank"].str.strip(" '\"")
+    df["rank"] = df["rank"].str.replace(" ", "_")
+    df["rank"] = df["rank"].str.lower()
+    return df
+
+def strip_leading_comma(df):
+    df["badge_no"] = df["badge_no"].str.strip(" '\"")
+    return df
+
 def clean():
     df = (
         pd.read_csv(deba.data("raw/levee_pd/levee_pd_pprr_1980_2025.csv"))
         .pipe(clean_column_names)
-        .drop(columns=['salary'])
+        .drop(columns=['salary','assignment'])
         .rename(columns={"year_of_birth": "birth_date", "gender":"sex", "date_of_hire": "hire_date", "date_of_termination":"termination_date", "badge_number": "badge_no"})
         .pipe(split_officer_name)
         .pipe(split_date_column, "birth_date")
         .pipe(split_date_column, "hire_date")
         .pipe(split_date_column, "termination_date")
         .pipe(strip_sex_and_race)
+        .pipe(strip_leading_comma)
+        .pipe(clean_rank)
         .pipe(lambda df: df.assign(
             race=df["race"].replace({
                 "b": "black",
@@ -60,7 +72,6 @@ def clean():
                 "m": "male",
                 "": pd.NA,
             })))
-        #.pipe(clean_rank, ["rank"])
         .pipe(set_values, {"agency": "orleans-levee-pd"})
         .pipe(gen_uid, ["agency", "first_name", "last_name"])
     )
