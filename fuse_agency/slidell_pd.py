07a95ae8ce2e3d5d44d7d8f9f20747870259d710
Esme Lee <esmelee@Esmes-MacBook-Air.local>
2025-06-26 11:51:45 -0500

    adding new slidell cprr 2007-2010 data to dvc and processing scripts

--- fuse_agency/slidell_pd.py
+++ fuse_agency/slidell_pd.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 import deba
-from lib.columns import rearrange_event_columns
+from lib.columns import rearrange_event_columns, rearrange_allegation_columns
 from lib.personnel import fuse_personnel
 from lib import events
 from lib.post import load_for_agency
 
 
-def fuse_events(pprr):
+def fuse_events(pprr,cprr):
     builder = events.Builder()
     builder.extract_events(
         pprr,
@@ -36,17 +36,30 @@ def fuse_events(pprr):
             },
         },
         ["uid"],
+    builder.extract_events(
+        cprr,
+        {
+            events.COMPLAINT_RECEIVE: {
+                "prefix": "receive",
+                "keep": ["agency", "allegation_uid", "uid"],
+            }
+        },
+        ["allegation_uid"],
+    )
     )
     return builder.to_frame(output_duplicated_events=True)
 
 
 if __name__ == "__main__":
     pprr_csd = pd.read_csv(deba.data("clean/pprr_slidell_csd_2010_2019.csv"))
+    cprr = pd.read_csv(deba.data("match/cprr_slidell_pd_2007_2010.csv"))
     agency = pprr_csd.agency[0]
     post = load_for_agency(agency)
     post_event = pd.read_csv(deba.data("match/post_event_slidell_pd_2020.csv"))
-    events_df = rearrange_event_columns(pd.concat([post_event, fuse_events(pprr_csd)]))
-    per_df = fuse_personnel(pprr_csd, post)
+    events_df = rearrange_event_columns(pd.concat([post_event, fuse_events(pprr_csd, cprr)]))
+    per_df = fuse_personnel(pprr_csd, post, cprr)
+    com = rearrange_allegation_columns(cprr)
     per_df.to_csv(deba.data("fuse_agency/per_slidell_pd.csv"), index=False)
     events_df.to_csv(deba.data("fuse_agency/event_slidell_pd.csv"), index=False)
+    com.to_csv(deba.data("fuse_agency/com_slidell_pd.csv"), index=False)
     post.to_csv(deba.data("fuse_agency/post_slidell_pd.csv"), index=False)
