cda0e30d5b44c4b7bb35d390b039c2c79733efd6
esmel200 <esmel@ip-no.org>
2025-10-16 14:14:41 -0400

    adding revising gretna scripts w new lines of code for the fuse_agency script

--- fuse_agency/gretna_pd.py
+++ fuse_agency/gretna_pd.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
 import deba
-from lib.columns import rearrange_personnel_columns, rearrange_event_columns
+from lib.columns import rearrange_personnel_columns, rearrange_event_columns, rearrange_allegation_columns
 from lib import events
 from lib.personnel import fuse_personnel
 from lib.post import load_for_agency
 
 
-def fuse_events(pprr, pprr25):
+def fuse_events(pprr, pprr25, cprr):
     builder = events.Builder()
     builder.extract_events(
         pprr,
@@ -18,8 +18,8 @@ def fuse_events(pprr, pprr25):
             }
         },
         ["uid"],
+        warn_duplications=True,
     )
-    builder = events.Builder()
     builder.extract_events(
         pprr25,
         {
@@ -29,6 +29,24 @@ def fuse_events(pprr, pprr25):
             }
         },
         ["uid"],
+        warn_duplications=True,
+    )
+    builder.extract_events(
+        cprr,
+        {
+            events.COMPLAINT_RECEIVE: {
+                "prefix": "receive",
+                "keep": [
+                    "uid",
+                    "allegation_uid",
+                    "agency",
+                    "allegation",
+                    "disposition",
+                    "tracking_id_og",
+                ],
+            },
+        },
+        ["uid", "allegation_uid"],
     )
     return builder.to_frame()
 
@@ -36,12 +54,16 @@ def fuse_events(pprr, pprr25):
 if __name__ == "__main__":
     pprr = pd.read_csv(deba.data("clean/pprr_gretna_pd_2018.csv"))
     pprr25 = pd.read_csv(deba.data("match/pprr_gretna_pd_2021_2025.csv"))
+    cprr = pd.read_csv(deba.data("match/cprr_gretna_pd_2020_2025.csv"))
     agency = pprr.agency[0]
     post = load_for_agency(agency)
     post_event = pd.read_csv(deba.data("match/post_event_gretna_pd_2020.csv"))
-    events_df = rearrange_event_columns(pd.concat([post_event, fuse_events(pprr, pprr25)]))
-    per_df = rearrange_personnel_columns(pd.concat([pprr, pprr25]))
-    per_df = fuse_personnel(per_df, post)
+    events_df = rearrange_event_columns(pd.concat([post_event, fuse_events(pprr, pprr25, cprr)]))
+    per_df = rearrange_personnel_columns(pd.concat([pprr, pprr25, cprr]))
+    com_df = rearrange_allegation_columns(cprr)
+    per_df = fuse_personnel(per_df, post, cprr)
     per_df.to_csv(deba.data("fuse_agency/per_gretna_pd.csv"), index=False)
+    com_df.to_csv(deba.data("fuse_agency/com_gretna_pd.csv"), index=False)
     events_df.to_csv(deba.data("fuse_agency/event_gretna_pd.csv"), index=False)
     post.to_csv(deba.data("fuse_agency/post_gretna_pd.csv"), index=False)
+ 
--- match/gretna_pd.py
+++ match/gretna_pd.py
@@ -81,13 +81,51 @@ def match_pprr_and_post(pprr25, post):
     pprr25.loc[:, "uid"] = pprr25.uid.map(lambda x: match_dict.get(x, x))
     return pprr25
 
+def match_cprr_and_post(cprr, post):
+    dfa = (
+        cprr.loc[cprr.uid.notna(), ["uid", "first_name", "last_name"]]
+        .drop_duplicates(subset=["uid"])
+        .set_index("uid", drop=True)
+    )
+    dfa.loc[:, "fc"] = dfa.first_name.fillna("").map(lambda x: x[:1])
+
+    dfb = (
+        post[["uid", "first_name", "last_name"]]
+        .drop_duplicates()
+        .set_index("uid", drop=True)
+    )
+    dfb.loc[:, "fc"] = dfb.first_name.fillna("").map(lambda x: x[:1])
+
+    matcher = ThresholdMatcher(
+        ColumnsIndex("fc"),
+        {
+            "first_name": JaroWinklerSimilarity(),
+            "last_name": JaroWinklerSimilarity(),
+        },
+        dfa,
+        dfb,
+    )
+    decision = 0.90
+    matcher.save_pairs_to_excel(
+        deba.data("match/cprr_gretna_2020_2025_v_post_pprr_08_25_2025.xlsx"),
+        decision,
+    )
+    matches = matcher.get_index_pairs_within_thresholds(decision)
+    match_dict = dict(matches)
+
+    cprr.loc[:, "uid"] = cprr.uid.map(lambda x: match_dict.get(x, x))
+    return cprr
+
 
 if __name__ == "__main__":
     pprr = pd.read_csv(deba.data("clean/pprr_gretna_pd_2018.csv"))
     pprr25 = pd.read_csv(deba.data("clean/pprr_gretna_pd_2021_2025.csv"))
+    cprr = pd.read_csv(deba.data("clean/cprr_gretna_pd_2020_2025.csv"))
     agency = pprr.agency[0]
     post = load_for_agency(agency)
     post_events = extract_post_events(pprr, post)
+    cprr = match_cprr_and_post(cprr, post)
     pprr25 = match_pprr_and_post(pprr25, post)
+    cprr.to_csv(deba.data("match/cprr_gretna_pd_2020_2025.csv"), index=False)
     post_events.to_csv(deba.data("match/post_event_gretna_pd_2020.csv"), index=False)
     pprr25.to_csv(deba.data("match/pprr_gretna_pd_2021_2025.csv"), index=False)
