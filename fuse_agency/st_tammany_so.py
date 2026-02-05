972804d81d499e5bfbcf9aac292db807c7e394ba
esmel200 <esmel@ip-no.org>
2026-02-05 14:10:23 -0600

    adding new stpso uof 2025 processing scripts

--- fuse_agency/st_tammany_so.py
+++ fuse_agency/st_tammany_so.py
@@ -1,12 +1,12 @@
 import deba
-from lib.columns import rearrange_allegation_columns, rearrange_event_columns
+from lib.columns import rearrange_allegation_columns, rearrange_citizen_columns, rearrange_event_columns
 from lib.personnel import fuse_personnel
 from lib import events
 import pandas as pd
 from lib.post import load_for_agency
 
 
-def fuse_events(pprr, cprr, pprr26):
+def fuse_events(pprr, cprr, pprr26, uof25):
     builder = events.Builder()
     builder.extract_events(
         pprr,
@@ -46,7 +46,22 @@ def fuse_events(pprr, cprr, pprr26):
         },
     },
     ["uid"],
-)
+    )
+    builder.extract_events(
+        uof25,
+        {
+            events.UOF_INCIDENT: {
+                "prefix": "occurred",
+                #"parse_date": True,
+                "keep": [
+                    "uid",
+                    "uof_uid",
+                    "agency",
+                ],
+            },
+        },
+        ["uid", "uof_uid"],
+    )
     return builder.to_frame()
 
 
@@ -54,14 +69,21 @@ if __name__ == "__main__":
     cprr = pd.read_csv(deba.data("match/cprr_st_tammany_so_2011_2021.csv"))
     pprr = pd.read_csv(deba.data("match/pprr_st_tammany_so_2020.csv"))
     pprr26 = pd.read_csv(deba.data("match/pprr_st_tammany_so_2026.csv"))
+    uof25 = pd.read_csv(deba.data("match/uof_st_tammany_so_2025.csv"))
+    citizen_df = pd.read_csv(deba.data("clean/uof_cit_st_tammany_so_2025.csv"))
+    #combined_cit = pd.concat([citizen_df, citizen_22_df], ignore_index=True)
+    citizen_df = rearrange_citizen_columns(citizen_df)
     agency = pprr.agency[0]
     post = load_for_agency(agency)
     post_event = pd.read_csv(deba.data("match/post_event_st_tammany_so_2020.csv"))
-    personnels = fuse_personnel(pprr, cprr, post, pprr26)
+    personnels = fuse_personnel(pprr, cprr, post, pprr26, uof25)
     complaints = rearrange_allegation_columns(cprr)
-    events_df = fuse_events(pprr, cprr, pprr26)
+    events_df = fuse_events(pprr, cprr, pprr26, uof25)
     events_df = rearrange_event_columns(pd.concat([post_event, events_df]))
     personnels.to_csv(deba.data("fuse_agency/per_st_tammany_so.csv"), index=False)
     events_df.to_csv(deba.data("fuse_agency/event_st_tammany_so.csv"), index=False)
     complaints.to_csv(deba.data("fuse_agency/com_st_tammany_so.csv"), index=False)
     post.to_csv(deba.data("fuse_agency/post_st_tammany_so.csv"), index=False)
+    uof25.to_csv(deba.data("fuse_agency/uof_st_tammany_so.csv"), index=False)
+    citizen_df.to_csv(deba.data("fuse_agency/cit_st_tammany_so.csv"), index=False)
+    
--- match/st_tammany_so.py
+++ match/st_tammany_so.py
@@ -172,20 +172,51 @@ def match_pprr26_and_post(pprr26, post):
     matches = matcher.get_index_pairs_within_thresholds(lower_bound=decision)
     return extract_events_from_post(post, matches, "st-tammany-so")
 
+def match_uof_and_post(uof25, post):
+    dfa = uof25[["uid", "first_name", "last_name"]]
+    dfa.loc[:, "fc"] = dfa.first_name.fillna("").map(lambda x: x[:1])
+    dfa = dfa.drop_duplicates(subset=["uid"]).set_index("uid")
+
+    dfb = post[["uid", "first_name", "last_name"]]
+    dfb.loc[:, "fc"] = dfb.first_name.fillna("").map(lambda x: x[:1])
+    dfb = dfb.drop_duplicates(subset=["uid"]).set_index("uid")
+
+    matcher = ThresholdMatcher(
+        ColumnsIndex(["fc"]),
+        {
+            "last_name": JaroWinklerSimilarity(),
+            "first_name": JaroWinklerSimilarity(),
+        },
+        dfa,
+        dfb,
+    )
+    decision = 0.89
+    matcher.save_pairs_to_excel(
+        deba.data("match/st_tammany_so_uof_2025_v_post_pprr_2025_08_25.xlsx"),
+        decision,
+    )
+    matches = matcher.get_index_pairs_within_thresholds(lower_bound=decision)
+    match_dict = dict(matches)
+    uof25.loc[:, "uid"] = uof25.uid.map(lambda x: match_dict.get(x, x))
+    post_events = extract_events_from_post(post, matches, "st-tammany-so")
+    return uof25, post_events
 
 if __name__ == "__main__":
     cprr = pd.read_csv(deba.data("clean/cprr_st_tammany_so_2011_2021.csv"))
     pprr = pd.read_csv(deba.data("clean/pprr_st_tammany_so_2020.csv"))
     pprr26 = pd.read_csv(deba.data("clean/pprr_st_tammany_so_2026.csv"))
+    uof25 = pd.read_csv(deba.data("clean/uof_st_tammany_so_2025.csv"))
     agency = pprr.agency[0]
     post = load_for_agency(agency)
     cprr = deduplicate_cprr(cprr)
     cprr = match_cprr(cprr, pprr)
     pprr26 = match_pprr26_pprr20(pprr26, pprr)
+    uof25, uof_post_events = match_uof_and_post(uof25, post)
     post_event = pd.concat(
-        [match_pprr_and_post(pprr, post), match_cprr_and_post(cprr, post), match_pprr26_and_post(pprr26, post)]
+        [match_pprr_and_post(pprr, post), match_cprr_and_post(cprr, post), uof_post_events]
     ).drop_duplicates(ignore_index=True)
     cprr.to_csv(deba.data("match/cprr_st_tammany_so_2011_2021.csv"), index=False)
     post_event.to_csv(deba.data("match/post_event_st_tammany_so_2020.csv"), index=False)
     pprr.to_csv(deba.data("match/pprr_st_tammany_so_2020.csv"), index=False)
     pprr26.to_csv(deba.data("match/pprr_st_tammany_so_2026.csv"), index=False)
+    uof25.to_csv(deba.data("match/uof_st_tammany_so_2025.csv"), index=False)
