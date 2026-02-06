431b624c9b9c3e05ee2e4145558ad77c7b98c012
esmel200 <esmel@ip-no.org>
2026-02-06 12:16:29 -0600

    fixing stpso uof scripts to add detail on front end cards

--- fuse_agency/st_tammany_so.py
+++ fuse_agency/st_tammany_so.py
@@ -1,5 +1,5 @@
 import deba
-from lib.columns import rearrange_allegation_columns, rearrange_citizen_columns, rearrange_event_columns
+from lib.columns import rearrange_allegation_columns, rearrange_citizen_columns, rearrange_event_columns, rearrange_use_of_force
 from lib.personnel import fuse_personnel
 from lib import events
 import pandas as pd
@@ -57,7 +57,8 @@ def fuse_events(pprr, cprr, pprr26, uof25):
                     "uid",
                     "uof_uid",
                     "agency",
-                    "use_of_force_type"
+                    "use_of_force_description",
+                    "use_of_force_reason",
                 ],
             },
         },
@@ -79,6 +80,7 @@ if __name__ == "__main__":
     post_event = pd.read_csv(deba.data("match/post_event_st_tammany_so_2020.csv"))
     personnels = fuse_personnel(pprr, cprr, post, pprr26, uof25)
     complaints = rearrange_allegation_columns(cprr)
+    uof25 = rearrange_use_of_force(uof25)
     events_df = fuse_events(pprr, cprr, pprr26, uof25)
     events_df = rearrange_event_columns(pd.concat([post_event, events_df]))
     personnels.to_csv(deba.data("fuse_agency/per_st_tammany_so.csv"), index=False)
